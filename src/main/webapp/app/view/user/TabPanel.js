24da044895384ec9debcad47b4e7e8f1df42abe1
agrt56 <andreas.gaertner@mni.thm.de>
2013-03-28 17:53:46 +0100

    Corrected misspelling.

--- src/main/webapp/app/view/user/TabPanel.js
+++ src/main/webapp/app/view/user/TabPanel.js
@@ -18,7 +18,7 @@
  along with this program; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  +--------------------------------------------------------------------------*/
-Ext.define('ARSnove.view.user.TabPanel', {
+Ext.define('ARSnova.view.user.TabPanel', {
 	extend: 'Ext.tab.Panel',
 	
 	config: {
