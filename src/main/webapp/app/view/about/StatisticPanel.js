3225f5bbac9b10afe556935dd196ab5f5448a585
agrt56 <andreas.gaertner@mni.thm.de>
2013-04-05 00:33:51 +0200

    Changed way to print statistics table. This was unavoidable, because sencha touch 2 don't support the way it was programmed anymore. See this comment: http://www.sencha.com/forum/showthread.php?160429-Dataview-itemtpl-vs-tpl

--- src/main/webapp/app/view/about/StatisticPanel.js
+++ src/main/webapp/app/view/about/StatisticPanel.js
@@ -19,13 +19,17 @@
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  +--------------------------------------------------------------------------*/
 Ext.define('ARSnova.view.about.StatisticPanel', {
-	extend: 'Ext.Panel',
+	extend: 'Ext.Container',
 	
 	config: {
 		title:	'StatisticPanel',
 		scroll: 'vertical',
-
-		gridPanel: null,
+		
+		/* panels */
+		tablePanel: null,
+		
+		/* statistics */
+		statistics: null,
 		
 		/* toolbar items */
 		toolbar		: null,
@@ -46,22 +50,14 @@ Ext.define('ARSnova.view.about.StatisticPanel', {
 	initialize: function() {
 		this.callParent(arguments);
 		
-		this.gridPanel = Ext.create('Ext.DataView', {
-	        store: Ext.create('Ext.data.Store', {
-	            model: ARSnova.model.Statistic
-	        }),
-	        tpl: Ext.create('Ext.XTemplate', 
-        		'<table class="statistic">',
-	        		'<tr><thead><th>' + Messages.CATEGORY + '</th><th>' + Messages.COUNT + '</th></thead></tr>',
-	        	    '<tpl for=".">',
-	        	    	'<tr><td>{category}</td><td>{counter}</td></tr>',
-		            '</tpl>',
-	            '<table></div>'
-	        ),
-	        itemSelector: 'div',
-	        scroll: false
-	    });
+	    this.statisticsStore = Ext.create('Ext.data.Store', {
+	    	model: 'ARSnova.model.Statistic'
+	    }),
 		
+	    this.tablePanel = Ext.create('Ext.Panel', {
+	    	html: ''
+	    }),
+	    
 		this.backButton = Ext.create('Ext.Button', {
 			text	: Messages.INFO,
 			ui		: 'back',
@@ -77,13 +73,13 @@ Ext.define('ARSnova.view.about.StatisticPanel', {
 			}
 		});
 		
-		this.toolbar = Ext.create('Toolbar', {
+		this.toolbar = Ext.create('Ext.Toolbar', {
 			title: Messages.STATISTIC,
 			docked: 'top',
 			items: [this.backButton]
 		});
 
-		this.add([this.gridPanel]);
+		this.add([this.toolbar, this.tablePanel]);	
 	
 		this.on('activate', this.onActivate);
 		this.on('deactivate', this.onDeactivate);
@@ -97,21 +93,53 @@ Ext.define('ARSnova.view.about.StatisticPanel', {
 		taskManager.stop(this.updateDataTask);
 	},
 	
-	getStatistics: function(){
+	buildTable: function() {
+	    if (this.statisticsStore != null) {
+	    	var htmlTemplate = '<div><table class="statistic"><tr><thead><th>' + Messages.CATEGORY + '</th>' +
+				'<th>' + Messages.COUNT + '</th></thead></tr>';
+	    	
+	    	this.statisticsStore.data.each(function(item) {
+	    		htmlTemplate += '<tr><td>' + item.data.category + '</td><td>' + item.data.counter + '</td></tr>';
+	        });
+	    	
+	        htmlTemplate += '</table></div>';
+	        this.tablePanel.setHtml(htmlTemplate);
+	    }
+	},
+	
+	/**
+	 * add statistics to store 'statisticsStore'
+	 */
+	addStatistics: function() {
+		if(this.statistics != null) {
+			this.statisticsStore.add({category: Messages.OPEN_SESSIONS, counter: this.statistics.openSessions});
+			this.statisticsStore.add({category: Messages.CLOSED_SESSIONS, counter: this.statistics.closedSessions});
+			this.statisticsStore.add({category: Messages.QUESTIONS, counter: this.statistics.questions});
+			this.statisticsStore.add({category: Messages.ANSWERS, counter: this.statistics.answers});
+			this.statisticsStore.add({category: "Users online", counter: this.statistics.activeUsers});
+	
+			this.statisticsStore.sort([{
+				property : 'category',
+				direction: 'DESC'
+			}]);
+		}
+	},
+	
+	/**
+	 * get statistics from proxy
+	 */
+	getStatistics: function() {
 		ARSnova.app.statisticModel.countSessions({
 			success: function(response){
 				var statistics = Ext.decode(response.responseText);
 				
-				var me = ARSnova.app.mainTabPanel.tabPanel.infoTabPanel.statisticPanel;
-				me.gridPanel.store.add({category: Messages.OPEN_SESSIONS, counter: statistics.openSessions});
-				me.gridPanel.store.add({category: Messages.CLOSED_SESSIONS, counter: statistics.closedSessions});
-				me.gridPanel.store.add({category: Messages.QUESTIONS, counter: statistics.questions});
-				me.gridPanel.store.add({category: Messages.ANSWERS, counter: statistics.answers});
-				me.gridPanel.store.add({category: "Users online", counter: statistics.activeUsers});
-				me.gridPanel.store.sort([{
-					property : 'category',
-					direction: 'DESC'
-				}]);
+				if(statistics != null) {
+					var me = ARSnova.app.mainTabPanel.tabPanel.infoTabPanel.statisticPanel;
+					me.statistics = statistics;
+					me.addStatistics();
+					me.buildTable();
+				}
+				
 				setTimeout("ARSnova.app.hideLoadMask()", 500);
 			},
 			failure: function(response){
@@ -122,7 +150,7 @@ Ext.define('ARSnova.view.about.StatisticPanel', {
 	
 	updateData: function(){
 		ARSnova.app.showLoadMask(Messages.LOAD_MASK);
-		this.gridPanel.store.clearData();
+		this.statisticsStore.clearData();
 		this.getStatistics();
 	}
 });
