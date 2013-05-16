1a5ce74e0b3db21da69a7275fa45a44776fbbafd
agrt56 <andreas.gaertner@mni.thm.de>
2013-05-16 07:30:00 +0200

    Heavy migration work and corrections.

--- src/main/webapp/app/view/Question.js
+++ src/main/webapp/app/view/Question.js
@@ -24,19 +24,6 @@ Ext.define('ARSnova.view.Question', {
 	config: {
 		scroll: 'vertical',
 		scrollable: true,
-		
-		listeners: {
-			preparestatisticsbutton: function(button) {
-				button.scope = this;
-				button.handler = function() {
-					var questionStatisticChart = Ext.create('ARSnova.view.QuestionStatisticChart', {
-						question	: this.questionObj,
-						lastPanel	: this
-					});
-					ARSnova.app.mainTabPanel.animateActiveItem(questionStatisticChart, 'slide');
-				};
-			}
-		},
 	},
 	
 	questionObj: null,
@@ -51,16 +38,29 @@ Ext.define('ARSnova.view.Question', {
 		this.questionObj = arguments.questionObj;
 		this.viewOnly = typeof arguments.viewOnly === "undefined" ? false : arguments.viewOnly;
 		
+		this.on('preparestatisticsbutton', function(button) {
+			button.scope = this;
+			button.setHandler(function() {
+				var questionStatisticChart = Ext.create('ARSnova.view.speaker.QuestionStatisticChart', {
+					question	: self.questionObj,
+					lastPanel	: self
+				});
+				ARSnova.app.mainTabPanel.animateActiveItem(questionStatisticChart, 'slide');
+			});
+		});
+		
 		answerStore.add(this.questionObj.possibleAnswers);
 
 		var questionListener = this.viewOnly ? {} : {
-			'itemtap': function(list, index, element, e) {
-				var answerObj 	= this.questionObj.possibleAnswers[index];
+			itemtap: function(list, index, element, record, e) {
+				var me = self;
+				var answerObj = me.questionObj.possibleAnswers[index];
 				
 				/* for use in Ext.Msg.confirm */
-				answerObj.selModel = list.selModel;
+				answerObj.selModel = list;
 				answerObj.target = e.target;
-				var theAnswer = answerObj.data.id || answerObj.data.text;
+
+				var theAnswer = answerObj.id || answerObj.text;
 				
 				Ext.Msg.confirm(
 					Messages.ANSWER + ' "' + theAnswer + '"', 
@@ -73,30 +73,31 @@ Ext.define('ARSnova.view.Question', {
 								answerObj.target = answerObj.target.parentElement;
 							}
 							
-							if (this.questionObj.showAnswer) {
-								if (answerObj.data.correct === 1 || answerObj.data.correct === true) {
+							if (me.questionObj.showAnswer) {
+								if (answerObj.correct === 1 || answerObj.correct === true) {
 									answerObj.target.className = "x-list-item x-list-item-correct";
 								} else {
-									for (var i = 0; i < this.questionObj.possibleAnswers.length; i++) {
-										var answer = this.questionObj.possibleAnswers[i].data;
+									for (var i = 0; i < me.questionObj.possibleAnswers.length; i++) {
+										var answer = me.questionObj.possibleAnswers[i];
 										if (answer.correct === 1 || answer.correct === true) {
 											list.element.dom.childNodes[i].className = "x-list-item x-list-item-correct";
 										}
 									}
 								}
 							}
-							
+
 							var saveAnswer = function(answer) {
 								answer.saveAnswer({
 									success: function() {
 										var questionsArr = Ext.decode(localStorage.getItem('questionIds'));
-										if (questionsArr.indexOf(this.questionObj._id) == -1) {
-											questionsArr.push(this.questionObj._id);
+										if (questionsArr.indexOf(me.questionObj._id) == -1) {
+											questionsArr.push(me.questionObj._id);
 										}
 										localStorage.setItem('questionIds', Ext.encode(questionsArr));
 										
 										list.up("panel").disable();
-										Ext.create('Ext.Panel', {
+										
+										var pnl = Ext.create('Ext.Panel', {
 											cls: 'notificationBox',
 											name: 'notificationBox',
 											showAnimation: 'pop',
@@ -104,22 +105,23 @@ Ext.define('ARSnova.view.Question', {
 											centered: true,
 											width: 300,
 											styleHtmlContent: true,
+											styleHtmlCls: 'notificationBoxText',
 											html: Messages.ANSWER_SAVED,
 											listeners: {
 												hide: function(){
 													this.destroy();
 												},
 												show: function(){
-													delayedFn = function(){
-														var cmp = Ext.ComponentQuery.query('panel[name=notificationBox]');
-														if(cmp.length > 0)
-															cmp[0].hide();
-														ARSnova.app.mainTabPanel.tabPanel.userQuestionsPanel.showNextUnanswered();
-													};
-													setTimeout("delayedFn()", 2000);
+													Ext.defer(function(){
+														pnl.hide();
+														var tP = ARSnova.app.mainTabPanel.tabPanel;
+														tP.userQuestionsPanel.showNextUnanswered();
+													}, 2000);
 												}
 											}
-										}).show();
+										});
+										Ext.Viewport.add(pnl);
+										pnl.show();
 									},
 									failure: function(response, opts) {
 										console.log('server-side error');
@@ -128,13 +130,13 @@ Ext.define('ARSnova.view.Question', {
 								});
 							};
 							
-							ARSnova.app.answerModel.getUserAnswer(this.questionObj._id, {
+							ARSnova.app.answerModel.getUserAnswer(me.questionObj._id, {
 								empty: function() {
 									var answer = Ext.create('ARSnova.model.Answer', {
 										type	 	: "skill_question_answer",
 										sessionId	: localStorage.getItem("sessionId"),
-										questionId	: this.questionObj._id,
-										answerText	: answerObj.data.text,
+										questionId	: me.questionObj._id,
+										answerText	: answerObj.text,
 										user		: localStorage.getItem("login")
 									});
 									
@@ -145,7 +147,7 @@ Ext.define('ARSnova.view.Question', {
 									
 									//update
 									var answer = Ext.create('ARSnova.model.Answer', theAnswer);
-									answer.set('answerText', answerObj.data.text);
+									answer.set('answerText', answerObj.text);
 									
 									saveAnswer(answer);
 								},
@@ -161,7 +163,7 @@ Ext.define('ARSnova.view.Question', {
 			}
 		};
 		
-		this.questionTitle = Ext.create('Ext.Component', {
+		this.questionTitle = Ext.create('Ext.Panel', {
 			cls: 'roundedBox',
 			html: 
 				'<p class="title">' + this.questionObj.subject + '<p/>' +
@@ -170,28 +172,28 @@ Ext.define('ARSnova.view.Question', {
 		
 		this.answerList = Ext.create('Ext.List', {
 			store	: answerStore,
-			
+
 			cls: 'roundedBox',
 			
 			scrollable: { disabled: true },
 			
 			itemTpl	: '{text}',
 		    listeners: {
-		    	questionListener: questionListener,
-		        initialize: function (list, eOpts){
+		    	initialize: function (list, eOpts){
 		            var me = this;
 		            if (typeof me.getItemMap == 'function'){
 		                me.getScrollable().getScroller().on('refresh',function(scroller,eOpts){
 		                    me.setHeight(me.getItemMap().getTotalHeight()+20);
 		                });
 		            }
-		        }
+		    	}
 		    }
 		});
 		
 		this.add([this.questionTitle, this.answerList]);
 		
 		this.on('activate', function(){
+			this.answerList.addListener('itemtap', questionListener.itemtap);
 			/*
 			 * Bugfix, because panel is normally disabled (isDisabled == true),
 			 * but is not rendered as 'disabled'
@@ -199,6 +201,13 @@ Ext.define('ARSnova.view.Question', {
 			if(this.isDisabled()) this.disable();
 		});
 	},
+	
+	//disable: function() {
+		//console.log(this);
+		//this.setMasked(true);
+		//console.log(this.activeItem);
+		//this.setScrollable(true);
+	//},
 	
 	decrementQuestionBadges: function() {
 		// Update badge inside the tab panel at the bottom of the screen
