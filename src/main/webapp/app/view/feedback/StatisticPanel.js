thm-projects/arsnova-mobile

commit 70341e7786d2a1295d42f1a520df050d5cbbc2c5

Migration of touch charts. (#6690, #6691, #6692)

src/main/webapp/app/view/feedback/StatisticPanel.js
@@ -18,15 +18,13 @@
  along with this program; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  +--------------------------------------------------------------------------*/
-feedbackChartColors = ['url(#v-3)', 'url(#v-2)', 'url(#v-1)', 'url(#v-4)'],
-
 Ext.define('ARSnova.view.feedback.StatisticPanel', {
 	extend: 'Ext.Panel',
 	
 	config: {
 		title: 'StatisticPanel',
 		style: 'background-color: black',
-		layout: 'vbox'
+		layout: 'fit'
 	},
 	
 	buttonClicked: null,
@@ -59,7 +57,7 @@ Ext.define('ARSnova.view.feedback.StatisticPanel', {
 				ARSnova.app.getController('Feedback').vote({
 					value : button.config.value
 				});
-			}
+			};
 		}
 		
 		this.toolbar = Ext.create('Ext.Toolbar', {
@@ -69,7 +67,8 @@ Ext.define('ARSnova.view.feedback.StatisticPanel', {
 		});
 
 		this.feedbackOkButton = Ext.create('Ext.Panel', {
-			cls: 'voteButtons left',
+			cls: 'voteButtons',
+			flex: 1,
 
 			items: [{
 				xtype		: 'button',
@@ -80,7 +79,8 @@ Ext.define('ARSnova.view.feedback.StatisticPanel', {
 		});
 
 		this.feedbackGoodButton = Ext.create('Ext.Panel', {
-			cls: 'voteButtons left',
+			cls: 'voteButtons',
+			flex: 1,
 			
 			items: [{
 				xtype		: 'button',
@@ -91,7 +91,8 @@ Ext.define('ARSnova.view.feedback.StatisticPanel', {
 		});
 		
 		this.feedbackBadButton = Ext.create('Ext.Panel', {
-			cls: 'voteButtons left',
+			cls: 'voteButtons',
+			flex: 1,
 			
 			items: [{
 				xtype		: 'button',
@@ -102,7 +103,8 @@ Ext.define('ARSnova.view.feedback.StatisticPanel', {
 		});
 		
 		this.feedbackNoneButton = Ext.create('Ext.Panel', {
-			cls: 'voteButtons left',
+			cls: 'voteButtons',
+			flex: 1,
 			
 			items: [{
 				xtype		: 'button',
@@ -112,123 +114,110 @@ Ext.define('ARSnova.view.feedback.StatisticPanel', {
 			}]
 		});
 
-		this.feedbackButtons = Ext.create('Ext.form.FormPanel', {
-			cls	 : 'actionsForm voteButtonsPanel',
-			scrollable: null,
-			layout: 'hbox',
-			flex: 1,
+		this.feedbackButtons = Ext.create('Ext.Toolbar', {
+			cls	 : 'voteButtonsPanel',
+			docked: 'top',
 			
 			items: [
-			    this.feedbackOkButton, {xtype: 'spacer'},
-			    this.feedbackGoodButton, {xtype: 'spacer'},
-			    this.feedbackBadButton, {xtype: 'spacer'},
+			    this.feedbackOkButton,
+			    this.feedbackGoodButton,
+			    this.feedbackBadButton,
 			    this.feedbackNoneButton
 		    ]
 		});
-
-		this.feedbackChart = Ext.create('Ext.chart.Chart', {
-			theme: 'Demo',
-			themeCls: 'column1',
-			flex: 20,
-			style: { marginTop: '40px' },
-		    store: Ext.create('Ext.data.JsonStore', {
-			    fields: ['name', 'displayName', 'value', 'percent'],
-			    data: [
-				  {'name': 'Kann folgen', 	 'displayName': Messages.FEEDBACK_OKAY, 'value': 0, 'percent': 0.0},
-		          {'name': 'Bitte schneller',  'displayName': Messages.FEEDBACK_GOOD,  'value': 0, 'percent': 0.0},
-		          {'name': 'Zu schnell', 		 'displayName': Messages.FEEDBACK_BAD, 'value': 0, 'percent': 0.0},
-		          {'name': 'Nicht mehr dabei', 'displayName': Messages.FEEDBACK_NONE, 'value': 0, 'percent': 0.0}
+		
+    	this.feedbackChartColors = [
+    	    Ext.create('Ext.draw.gradient.Linear', {
+    	    	degrees: 90,
+    	    	stops: [{ offset: 0,	color: 'rgb(122, 184, 68)' },
+    	    	        { offset: 100,	color: 'rgb(82, 144, 28)' }
+    	    	]
+    	    }),
+		
+    	    Ext.create('Ext.draw.gradient.Linear', {
+    	    	degrees: 90,
+		        stops: [{ offset: 0, 	color: 'rgb(254, 201, 41)' },
+		                { offset: 100,	color: 'rgb(214, 161, 0)' }
 		        ]
 			}),
-
+			
+			Ext.create('Ext.draw.gradient.Linear', {
+				degrees: 90,
+		        stops: [{offset: 0,		color: 'rgb(237, 96, 28)' },
+		                {offset: 100,	color: 'rgb(197, 56, 0)' }
+		        ]
+			}),
+			
+			Ext.create('Ext.draw.gradient.Linear', {
+				degrees: 90,
+		    	stops: [{ offset: 0,	color: 'rgb(235, 235, 235)' },
+		    	        { offset: 100, 	color: 'rgb(195,195,195)' }
+		        ]
+			})
+		];
+    	
+		this.feedbackChart = Ext.create('Ext.chart.CartesianChart', {
+	    	store:  Ext.create('Ext.data.Store', {
+	    		fields: ['name', 'displayName', 'value', 'percent'],
+	    		data: [
+	    		       {'name': 'Kann folgen',		'displayName': Messages.FEEDBACK_OKAY,	'value': 0, 'percent': 0.0},
+	    		       {'name': 'Bitte schneller',	'displayName': Messages.FEEDBACK_GOOD,	'value': 0, 'percent': 0.0},
+	    		       {'name': 'Zu schnell',		'displayName': Messages.FEEDBACK_BAD,	'value': 0, 'percent': 0.0},
+	    		       {'name': 'Nicht mehr dabei',	'displayName': Messages.FEEDBACK_NONE,	'value': 0, 'percent': 0.0}
+	    		]
+	    	}),
+	    	
 		    animate: {
 		        easing: 'bounceOut',
 		        duration: 750
 		    },
-
+		    
 		    axes: [{
 		        type: 'numeric',
 		        position: 'left',
 		        fields: ['value'],
-		        hidden: true,
-		        minimum: 0,
-		        grid: {
-			        opacity: 0
-		        }
+		        hidden: true
 		    }, {
 		        type: 'category',
 		        position: 'bottom',
-		        fields : ['displayName'],
-		        label: {
-		            renderer: function(v) {
-		                return '';
-		            }
+		        fields : ['name'],
+		        style: { stroke : 'white' },
+		        renderer: function(label, layout, lastLabel) {
+		        	// remove x-axis ticks and labels on refresh or update
+		        	layout.attr.majorTicks = false;
 		        }
 		    }],
 		    
-		    gradients: [{
-		    	'id': 'v-1',
-		        'angle': 0,
-		        stops: {
-		            0:   { color: 'rgb(237, 96, 28)' },
-		            100: { color: 'rgb(197, 56, 0)' }
-		        }
-		    },
-		    {
-		        'id': 'v-2',
-		        'angle': 0,
-		        stops: {
-		            0:   { color: 'rgb(254, 201, 41)'},
-		            100: { color: 'rgb(214, 161, 0)' }
-		        }
-		    },
-		    {
-		        'id': 'v-3',
-		        'angle': 0,
-		        stops: {
-		            0:   { color: 'rgb(122, 184, 68)' },
-		            100: { color: 'rgb(82, 144, 28)' }
-		        }
-		    },
-		    {
-		        'id': 'v-4',
-		        'angle': 0,
-		        stops: {
-		            0:   { color: 'rgb(235, 235, 235)' },
-		            100: { color: 'rgb(195,195,195)' }
-		        }
-		    }],
-
 		    series: [{
-		        type: 'column',
-		        axis: 'left',
+		        type: 'bar',
 		        xField: 'name',
 		        yField: 'value',
-		        highlight: true,
-		        renderer: function(sprite, storeItem, barAttr, i, store) {
-		            barAttr.fill = feedbackChartColors[i % feedbackChartColors.length];
-		            return barAttr;
-		        },
-		        label: {
-		            renderer: function(v) {
-		                return '';
-		            }
-		        },
 		        style: {
-		            color:0x6238A7, 
-		            size:8,
-		            fill: 'blue'
-		        }
-		    }]
+		        	minGapWidth: 25,
+		        	maxBarWidth: 200
+		        },
+		        renderer: function (sprite, config, rendererData, i) {		 
+		        	var panel = ARSnova.app.mainTabPanel.tabPanel.feedbackTabPanel.statisticPanel;
+
+		        	return rendererData = { 
+		        			fill : panel.feedbackChartColors[i % panel.feedbackChartColors.length] 
+		        	};
+		        },
+		    }],
 		});
-		
+
 		this.add([this.toolbar, this.feedbackButtons, this.feedbackChart]);
+		
+		this.onBefore('activate', function() {
+			// remove x-axis ticks and labels at initialization
+			this.feedbackChart.getAxes()[1].sprites[0].attr.majorTicks = false;
+		});
 	},
 	
 	updateChart: function(feedbackValues) {
 		var chart = this.feedbackChart;
 		var store = chart.getStore();
-		
+
 		/* Swap values for "can follow" and "faster, please" feedback
 		 * TODO: improve implementation, this is a quick hack for MoodleMoot 2013 */
 		var values = feedbackValues.slice();
@@ -245,10 +234,10 @@ Ext.define('ARSnova.view.feedback.StatisticPanel', {
 		// Calculate percentages
 		var sum = store.sum('value');
 		store.each(function(record) {
-			record.set('percent', sum > 0 ? (record.data.value / sum) : 0.0);
+			record.set('percent', sum > 0 ? (record.get('value') / sum) : 0.0);
 		});
 		
-		chart._axes.items[0]._maximum = Math.max.apply(null, values);
+		chart.getAxes()[0].setMaximum(Math.max.apply(null, values));
 		chart.redraw();
 	},
 	

src/main/webapp/app/view/speaker/QuestionStatisticChart.js
@@ -18,18 +18,17 @@
  along with this program; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  +--------------------------------------------------------------------------*/
-questionChartColors = ['url(#v1)', 'url(#v2)', 'url(#v3)', 'url(#v4)', 'url(#v5)', 'url(#v6)'],
-
 Ext.define('ARSnova.view.speaker.QuestionStatisticChart', {
 	extend: 'Ext.Panel',
 	
 	config: {
 		title	: Messages.STATISTIC,
-		style: 'background-color: black',
+		style	: 'background-color: black',
 		iconCls	: 'tabBarIconCanteen',
 		layout	: 'fit'
 	},
 	
+	gradients: null,
 	questionObj: null,
 	questionChart: null,
 	questionStore: null,
@@ -112,8 +111,14 @@ Ext.define('ARSnova.view.speaker.QuestionStatisticChart', {
 				xtype: 'container',
 				cls: "x-toolbar-title counterText",
 				html: "0/0",
-				style: {paddingRight: '10px'}
+				style: { paddingRight: '10px' }
 			}]
+		});
+		
+		this.titlebar = Ext.create('Ext.Toolbar', {
+			cls		: 'questionStatisticTitle',
+			docked	: 'top',
+			title: title
 		});
 		
 		if( this.questionObj.questionType == "yesno" 	|| 
@@ -126,121 +131,107 @@ Ext.define('ARSnova.view.speaker.QuestionStatisticChart', {
 					var question = this.questionObj.possibleAnswers[i];
 					
 					if ((question.data && !question.data.correct) || (!question.data && !question.correct)){
-						this.gradients.push({
-							'id': 'v' + (i+1),
-							'angle': 0,
-							stops: {
-								0:   { color: 'rgb(212, 40, 40)' },
-								100: { color: 'rgb(117, 14, 14)' }
-							}
-						});
+						this.gradients.push(
+							Ext.create('Ext.draw.gradient.Linear', {
+								degrees: 90,
+								stops: [{ offset: 0,	color: 'rgb(212, 40, 40)' },
+								        { offset: 100,	color: 'rgb(117, 14, 14)' }
+								]
+							})
+						);
 					} else {
-						this.gradients.push({
-							'id': 'v' + (i+1),
-							'angle': 0,
-							stops: {
-								0:   { color: 'rgb(43, 221, 115)' },
-								100: { color: 'rgb(14, 117, 56)' }
-							}
-						});
-					}
-						
+						this.gradients.push(
+							Ext.create('Ext.draw.gradient.Linear', {
+								degrees: 90,
+								stops: [{ offset: 0,	color: 'rgb(43, 221, 115)'  },
+								        { offset: 100,	color: 'rgb(14, 117, 56)' }
+								]
+							})
+						);
+					}		
 				}
 			} else {
-				this.gradients = [{
-					'id': 'v1',
-					'angle': 0,
-					stops: {
-						0:   { color: 'rgb(22, 64, 128)' },
-						100: { color: 'rgb(0, 14, 88)' }
-					}
-				}, {
-					'id': 'v2',
-					'angle': 0,
-					stops: {
-						0:   { color: 'rgb(48, 128, 128)' },
-						100: { color: 'rgb(8, 88, 88)' }
-					}
-				}, {
-					'id': 'v3',
-					'angle': 0,
-					stops: {
-						0:   { color: 'rgb(128, 128, 25)' },
-						100: { color: 'rgb(88, 88, 0)' }
-					}
-				}, {
-					'id': 'v4',
-					'angle': 0,
-					stops: {
-						0:   { color: 'rgb(128, 28, 128)' },
-						100: { color: 'rgb(88, 0, 88)' }
-					}
-				}, {
-					'id': 'v5',
-					'angle': 0,
-					stops: {
-						0:   { color: 'rgb(128, 21, 21)' },
-						100: { color: 'rgb(88, 0, 0)' }
-					}
-				}, {
-					'id': 'v6',
-					'angle': 0,
-					stops: {
-						0:   { color: 'rgb(128, 64, 22)' },
-						100: { color: 'rgb(88, 24, 0)' }
-					}
-				}];
+				this.gradients = [
+					Ext.create('Ext.draw.gradient.Linear', {
+						degrees: 90,
+						stops: [{ offset: 0,	color: 'rgb(22, 64, 128)'  },
+						        { offset: 100,	color: 'rgb(0, 14, 88)' }
+						]
+					}),
+					Ext.create('Ext.draw.gradient.Linear', {
+						degrees: 90,
+						stops: [{ offset: 0,	color: 'rgb(48, 128, 128)'  },
+						        { offset: 100,	color: 'rgb(8, 88, 88)' }
+						]
+					}),
+					Ext.create('Ext.draw.gradient.Linear', {
+						degrees: 90,
+						stops: [{ offset: 0,	color: 'rgb(128, 128, 25)'  },
+						        { offset: 100,	color: 'rgb(88, 88, 0)' }
+						]
+					}),
+					Ext.create('Ext.draw.gradient.Linear', {
+						degrees: 90,
+						stops: [{ offset: 0,	color: 'rgb(128, 28, 128)' },
+						        { offset: 100,	color: 'rgb(88, 0, 88)' }
+						]
+					}),
+					Ext.create('Ext.draw.gradient.Linear', {
+						degrees: 90,
+						stops: [{ offset: 0,	color: 'rgb(128, 21, 21)' },
+						        { offset: 100,	color: 'rgb(88, 0, 0)' }
+						]
+					}),
+					Ext.create('Ext.draw.gradient.Linear', {
+						degrees: 90,
+						stops: [{ offset: 0,	color: 'rgb(128, 64, 22)' },
+						        { offset: 100,	color: 'rgb(88, 24, 0)' }
+						]
+					})
+				];
 			}
 		} else {
-			this.gradients = [{
-				'id': 'v1',
-				'angle': 0,
-				stops: {
-					0:   { color: 'rgb(22, 64, 128)' },
-					100: { color: 'rgb(0, 14, 88)' }
-				}
-			}, {
-				'id': 'v2',
-				'angle': 0,
-				stops: {
-					0:   { color: 'rgb(48, 128, 128)' },
-					100: { color: 'rgb(8, 88, 88)' }
-				}
-			}, {
-				'id': 'v3',
-				'angle': 0,
-				stops: {
-					0:   { color: 'rgb(128, 128, 25)' },
-					100: { color: 'rgb(88, 88, 0)' }
-				}
-			}, {
-				'id': 'v4',
-				'angle': 0,
-				stops: {
-					0:   { color: 'rgb(128, 28, 128)' },
-					100: { color: 'rgb(88, 0, 88)' }
-				}
-			}, {
-				'id': 'v5',
-				'angle': 0,
-				stops: {
-					0:   { color: 'rgb(128, 21, 21)' },
-					100: { color: 'rgb(88, 0, 0)' }
-				}
-			}, {
-				'id': 'v6',
-				'angle': 0,
-				stops: {
-					0:   { color: 'rgb(128, 64, 22)' },
-					100: { color: 'rgb(88, 24, 0)' }
-				}
-			}];
+			this.gradients = [
+				Ext.create('Ext.draw.gradient.Linear', {
+					degrees: 90,
+					stops: [{ offset: 0,	color: 'rgb(22, 64, 128)' },
+					        { offset: 100,	color: 'rgb(0, 14, 88)' }
+					]
+				}),
+				Ext.create('Ext.draw.gradient.Linear', {
+					degrees: 90,
+					stops: [{ offset: 0,	color: 'rgb(48, 128, 128)' },
+					        { offset: 100,	color: 'rgb(8, 88, 88)' }
+					]
+				}),
+				Ext.create('Ext.draw.gradient.Linear', {
+					degrees: 90,
+					stops: [{ offset: 0,	color: 'rgb(128, 128, 25)' },
+					        { offset: 100,	color: 'rgb(88, 88, 0)' }
+					]
+				}),
+				Ext.create('Ext.draw.gradient.Linear', {
+					degrees: 90,
+					stops: [{ offset: 0,	color: 'rgb(128, 28, 128)' },
+					        { offset: 100,	color: 'rgb(88, 0, 88)' }
+					]
+				}),
+				Ext.create('Ext.draw.gradient.Linear', {
+					degrees: 90,
+					stops: [{ offset: 0,	color: 'rgb(128, 21, 21)' },
+					        { offset: 100,	color: 'rgb(88, 0, 0)' }
+					]
+				}),
+				Ext.create('Ext.draw.gradient.Linear', {
+					degrees: 90,
+					stops: [{ offset: 0,	color: 'rgb(128, 64, 22)' },
+					        { offset: 100,	color: 'rgb(88, 24, 0)' }
+					]
+				}),
+			];
 		}
 		
-		this.questionChart = Ext.create('Ext.chart.Chart', {
-		    theme: 'Demo',
-			themeCls: 'column1',
-
+		this.questionChart = Ext.create('Ext.chart.CartesianChart', {
 		    store: this.questionStore,
 
 		    animate: {
@@ -248,60 +239,55 @@ Ext.define('ARSnova.view.speaker.QuestionStatisticChart', {
 		        duration: 1000
 		    },
 		    
-		    gradients: this.gradients,
-		    
 		    axes: [{
-		        type: 'Numeric',
+		        type	: 'numeric',
 		        position: 'left',
-		        fields: ['value'],
+		        fields	: ['value'],
 		        minimum: 0,
-		        maximum: this.maxValue,
-		        label: {
-		            renderer: function(v) {
-		                return v.toFixed(0);
-		            }
-		        }
-		    },
-		    {
-		        type: 'Category',
-		        position: 'bottom',
-		        fields: ['text'],
-		        label: {
-		        	rotate: {
-		        		degrees: 315
-		        	}
-		        }
+		        style: { stroke: 'white' },
+		        label: { strokeStyle: 'white' }
 		    }, {
-	            type    : 'Category',
-	            position: 'top',
-	            label   : {
-	            	renderer: function(){
-	            		return "";
-	            	}
-            	},
-	            title   : title,
-	            dashSize: 0
-	        } ],
-		    series: [{
-		        type: 'column',
-		        axis: 'left',
-		        highlight: true,
-		        renderer: function(sprite, storeItem, barAttr, i, store) {
-		            barAttr.fill = questionChartColors[i % questionChartColors.length];
-		            return barAttr;
-		        },
+		        type	: 'category',
+		        position: 'bottom',
+		        fields	: ['text'],
+		        style: { stroke: 'white' },
 		        label: {
-		          field: 'percent',
-		          renderer: function(v) {
-		        	  return Math.round(v * 100) + "%";
-		          }
-		        },
+		        	strokeStyle: 'white',
+		        	rotate: { degrees: 315}
+		        }
+		    }],
+	        
+		    series: [{
+		        type: 'bar',
 		        xField: 'text',
-		        yField: 'value'
+		        yField: 'value',
+		        renderer: function (sprite, config, rendererData, i) {		 
+		        	var panel;
+		        	
+		    		if(ARSnova.app.userRole == ARSnova.app.USER_ROLE_STUDENT) {
+						panel = ARSnova.app.mainTabPanel.tabPanel.userQuestionsPanel.questionStatisticChart;
+		    		}
+		    		
+		    		else {
+		    			panel = ARSnova.app.mainTabPanel.tabPanel.speakerTabPanel.questionStatisticChart;
+		    		}
+
+		        	return rendererData = {
+		        			fill : panel.gradients[i % panel.gradients.length]
+		        	};
+		        },
+		        style: {
+		        	minGapWidth: 25,
+		        	maxBarWidth: 200
+		        },
+		        labelField: ['percent'],
+		        label: {
+		        	strokeStyle: 'white'
+		        }
 		    }]
 		});
 		
-		this.add([this.toolbar, this.questionChart]);
+		this.add([this.toolbar, this.titlebar, this.questionChart]);
 
 		this.on('activate', this.onActivate);
 	},
@@ -322,7 +308,7 @@ Ext.define('ARSnova.view.speaker.QuestionStatisticChart', {
 				for ( var i = 0; i < tmp_possibleAnswers.length; i++) {
 					var el = tmp_possibleAnswers[i];
 					var record = store.findRecord('text', el, 0, false, true, true);
-					record.data.value = 0;
+					record.set('value', 0);
 				}
 				
 				for ( var i = 0; i < panel.questionObj.possibleAnswers.length; i++) {
@@ -367,7 +353,7 @@ Ext.define('ARSnova.view.speaker.QuestionStatisticChart', {
 							continue;
 						}
 						var record = store.findRecord('text', el.answerText, 0, false, true, true); //exact match
-						record.data.value = el.answerCount;
+						record.set('value', el.answerCount);
 					}
 					sum += el.answerCount;
 					
@@ -390,9 +376,10 @@ Ext.define('ARSnova.view.speaker.QuestionStatisticChart', {
 				// Calculate percentages
 				var totalResults = store.sum('value');
 				store.each(function(record) {
-					record.data.percent = totalResults > 0 ? (record.data.value / totalResults) : 0.0;
+					var percent = Math.round(record.get('value') / totalResults) * 100;
+					record.set('percent', totalResults > 0 && percent > 0 ? percent + ' %' : 0.0);
 				});
-				chart._axes.items[0]._maximum = maxValue;
+				chart.getAxes()[0].setMaximum(maxValue);
 				
 				// renew the chart-data
 				chart.redraw();
@@ -414,7 +401,6 @@ Ext.define('ARSnova.view.speaker.QuestionStatisticChart', {
 		taskManager.start(this.renewChartDataTask);
 		taskManager.start(this.countActiveUsersTask);
 		
-		this.questionChart.getAxes().items[2].style.stroke = "#0E0E0E";
 		this.questionChart.redraw();
 	},
 	

src/main/webapp/app/view/Question.js
@@ -45,11 +45,12 @@ Ext.define('ARSnova.view.Question', {
 		this.on('preparestatisticsbutton', function(button) {
 			button.scope = this;
 			button.setHandler(function() {
-				var questionStatisticChart = Ext.create('ARSnova.view.speaker.QuestionStatisticChart', {
+				var panel = ARSnova.app.mainTabPanel.tabPanel.userQuestionsPanel;
+				panel.questionStatisticChart = Ext.create('ARSnova.view.speaker.QuestionStatisticChart', {
 					question	: self.questionObj,
 					lastPanel	: self
 				});
-				ARSnova.app.mainTabPanel.animateActiveItem(questionStatisticChart, 'slide');
+				ARSnova.app.mainTabPanel.animateActiveItem(panel.questionStatisticChart, 'slide');
 			});
 		});
 		

src/main/webapp/app/view/user/QuestionPanel.js
@@ -30,6 +30,7 @@ Ext.define('ARSnova.view.user.QuestionPanel', {
 	/* toolbar items */
 	toolbar		: null,
 	backButton	: null,
+	chartPanel	: null,
 	questionCounter: 0,
 	
 	/* item index 0 and 1 are occupied by the carousel and toolbar. */
@@ -105,14 +106,7 @@ Ext.define('ARSnova.view.user.QuestionPanel', {
 		this.statisticButton = Ext.create('Ext.Button', {
 			text	: ' ',
 			cls		: 'statisticIconSmall',
-			hidden	: true,
-			handler	: function() {
-				var questionStatisticChart = Ext.create('ARSnova.view.speaker.QuestionStatisticChart', {
-					question: ARSnova.app.mainTabPanel.tabPanel.userQuestionsPanel._activeItem.questionObj,
-					lastPanel: this
-				});
-				ARSnova.app.mainTabPanel.animateActiveItem(questionStatisticChart, 'slide');
-			}
+			hidden	: true
 		});
 		
 		this.toolbar = Ext.create('Ext.Toolbar', {

src/main/webapp/developer.html
@@ -25,9 +25,6 @@
 		 <script type="text/javascript" src="app/utils/st2-Ext.plugins.ResizableTextArea.js"></script>
 		 <script type="text/javascript" src="app/utils/Ext.Array.js"></script>
 		
-		 <!-- touch-charts -->
-		 <script type="text/javascript" src="lib/touch-charts-2.0/touch-charts-debug.js"></script>
-		
 		 <script type="text/javascript" src="lib/rsvp.min.js"></script>
 		 <script type="text/javascript" src="lib/socket.io/socket.io.min.js"></script>
 		 <script type="text/javascript" src="https://c328740.ssl.cf1.rackcdn.com/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>

src/main/webapp/index.html
@@ -29,7 +29,6 @@
 		
 		<script type="text/javascript" src="app/st2-app.js"></script>
 		
-		<script type="text/javascript" src="lib/touch-charts-2.0/touch-charts.js"></script>
 		<script type="text/javascript" src="lib/rsvp.min.js"></script>
 		<script type="text/javascript" src="lib/socket.io/socket.io.min.js"></script>
 		<script type="text/javascript" src="https://c328740.ssl.cf1.rackcdn.com/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>

src/main/webapp/resources/css/st2-arsnova_new.css
@@ -10,6 +10,11 @@
 	width: 0;
 }
 
+/* remove "Sencha Touch GPL3" watermark from charts */
+.x-chart-watermark {
+	opacity: 0
+}
+
 /* Overwriting css classes*/
 .x-input-text, .x-input-password {
 	text-align: center;
@@ -1187,17 +1192,21 @@ ol.standardList li {
 /* END for feedback questions */
 
 /* BEGIN of feedback vote */
-.voteButtons {
-	width: 19.3%;
-}
-
 .voteButtons > div {
 	padding: 0 !important;
 }
 
 .voteButtonsPanel {
-	margin: 20px;
-	margin-bottom: 0px;
+	background: black;
+	padding-left: 10px;
+	padding-right: 10px;
+	margin-top: 15px;
+	margin-bottom: 5px;
+}
+
+.questionStatisticTitle {
+	background: black;
+	font-size: 80% !important;
 }
 
 .feedbackOkIcon {
@@ -1206,9 +1215,9 @@ ol.standardList li {
 		no-repeat !important;
 	width: 48px !important;
 	min-height: 48px !important;
-	border: none;
+	border: none !important;
 	background-position: center;
-	margin: 0pt auto;
+	margin: 0pt auto !important;
 }
 
 .feedbackGoodIcon {
@@ -1217,9 +1226,9 @@ ol.standardList li {
 		no-repeat !important;
 	width: 48px !important;
 	min-height: 48px !important;
-	border: none;
+	border: none !important;
 	background-position: center;
-	margin: 0pt auto;
+	margin: 0pt auto !important;
 }
 
 .feedbackBadIcon {
@@ -1228,9 +1237,9 @@ ol.standardList li {
 		no-repeat !important;
 	width: 48px !important;
 	min-height: 48px !important;
-	border: none;
+	border: none !important;
 	background-position: center;
-	margin: 0pt auto;
+	margin: 0pt auto !important;
 }
 
 .feedbackNoneIcon {
@@ -1239,9 +1248,9 @@ ol.standardList li {
 		no-repeat !important;
 	width: 48px !important;
 	min-height: 48px !important;
-	border: none;
+	border: none !important;
 	background-position: center;
-	margin: 0pt auto;
+	margin: 0pt auto !important;
 }
 
 .feedbackOkBackground .iconBtn {

commit 49ec830ef81711b13081142726a1f60ff7d6f209

Fix broken build caused by syntax errors introduced in 70341e7

src/main/webapp/app/view/feedback/StatisticPanel.js
@@ -202,8 +202,8 @@ Ext.define('ARSnova.view.feedback.StatisticPanel', {
 		        	return rendererData = { 
 		        			fill : panel.feedbackChartColors[i % panel.feedbackChartColors.length] 
 		        	};
-		        },
-		    }],
+		        }
+		    }]
 		});
 
 		this.add([this.toolbar, this.feedbackButtons, this.feedbackChart]);

src/main/webapp/app/view/speaker/QuestionStatisticChart.js
@@ -227,7 +227,7 @@ Ext.define('ARSnova.view.speaker.QuestionStatisticChart', {
 					stops: [{ offset: 0,	color: 'rgb(128, 64, 22)' },
 					        { offset: 100,	color: 'rgb(88, 24, 0)' }
 					]
-				}),
+				})
 			];
 		}
 		