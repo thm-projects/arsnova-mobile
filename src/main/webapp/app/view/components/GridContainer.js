980a9c3961bab0b37a40e1f7976fc8257c354832
Sören Gutzeit <soeren.gutzeit@mni.thm.de>
2014-02-02 19:34:17 +0100

    Mehrfachauswahl und un-Auswahl für Felder im Gitter jetzt möglich

--- src/main/webapp/app/view/components/GridContainer.js
+++ src/main/webapp/app/view/components/GridContainer.js
@@ -1,155 +1,166 @@
-Ext.define('ARSnova.view.components.GridContainer', {
-	extend : 'Ext.Container',
+Ext.define('ARSnova.view.components.GridContainer',
+		{
+			extend : 'Ext.Container',
 
-	config : {
-		gridSize : 5,
-		imgSize : 400,
-		canvas : null,
-		imageFile : null,
-		gridWidth : 1,
-	},
+			config : {
+				gridSize : 5,
+				imgSize : 400,
+				canvas : null,
+				imageFile : null,
+				gridWidth : 1,
+				maxChosenFields : 1,
+				chosenFields : Array()
+			},
 
-	constructor : function() {
-		this.callParent(arguments);
+			constructor : function() {
+				this.callParent(arguments);
 
-		var canvas = document.createElement('canvas');
-		canvas.id = 'canvasWrapper';
-		canvas.width = this.getImgSize();
-		canvas.height = this.getImgSize();
-		canvas.style.display = 'block';
-		canvas.style.margin = '0 auto';
-		canvas.addEventListener("mousedown", this.onclick, false);
-		canvas.parentContainer = this;
+				var canvas = document.createElement('canvas');
+				canvas.id = 'canvasWrapper';
+				canvas.width = this.getImgSize();
+				canvas.height = this.getImgSize();
+				canvas.style.display = 'block';
+				canvas.style.margin = '0 auto';
+				canvas.addEventListener("mousedown", this.onclick, false);
+				canvas.parentContainer = this;
 
-		var newimage = new Image();
-		var container = this;
+				var newimage = new Image();
+				var container = this;
 
-		newimage.onload = function() {
-			var ctx = canvas.getContext('2d');
-			canvas.getContext('2d').drawImage(newimage, 0, 0);
-			container.setImageFile(newimage);
-			container.createGrid();
-		}
+				newimage.onload = function() {
+					var ctx = canvas.getContext('2d');
+					canvas.getContext('2d').drawImage(newimage, 0, 0);
+					container.setImageFile(newimage);
+					container.createGrid();
+				}
 
-		newimage.src = '../resources/images/planquadrat_test.jpg';
+				newimage.src = '../resources/images/planquadrat_test.jpg';
 
-		this.setCanvas(canvas);
+				this.setCanvas(canvas);
 
-		this.image = {
-			xtype : 'panel',
-			cls : null,
-			html : canvas,
-		}
+				this.image = {
+					xtype : 'panel',
+					cls : null,
+					html : canvas,
+				}
 
-		this.add([ this.image ]);
-	},
+				this.add([ this.image ]);
+			},
 
-	clearAll : function() {
-		var ctx = this.getCanvas().getContext('2d');
-		ctx.clearRect(0, 0, this.getCanvas().width, this.getCanvas().height);
-		ctx.globalAlpha = 1;
-		ctx.drawImage(this.getImageFile(), 0, 0);
-		console.log('cleared.')
-	},
+			clearAll : function() {
+				var ctx = this.getCanvas().getContext('2d');
+				ctx.clearRect(0, 0, this.getCanvas().width,
+						this.getCanvas().height);
+				ctx.globalAlpha = 1;
+				ctx.drawImage(this.getImageFile(), 0, 0);
+				console.log('cleared.')
+			},
 
-	getFieldKoord : function(x, y) {
+			// get the posotion params of a field by the koords of the click
+			// event
+			getFieldPosition : function(x, y) {
+				var canvas = this.getCanvas();
 
-		var canvas = this.getCanvas();
+				x -= canvas.getBoundingClientRect().left;
+				y -= canvas.getBoundingClientRect().top;
 
-		x -= canvas.getBoundingClientRect().left;
-		y -= canvas.getBoundingClientRect().top;
+				var xGrid = parseInt(x / (this.getImgSize()/this.getGridSize()));
+				var yGrid = parseInt(y / (this.getImgSize()/this.getGridSize()));
+				return new Array(xGrid, yGrid);
+			},
 
-		var xGrid = parseInt(x / this.getFieldSize());
-		var yGrid = parseInt(y / this.getFieldSize());
+			// get relative start koords of a field by its position params
+			getFieldKoord : function(x, y) {
+				var x1 = x * this.getFieldSize() + 2 * this.getGridWidth();
+				var y1 = y * this.getFieldSize() + 2 * this.getGridWidth();
+				return new Array(x1, y1);
+			},
 
-		// +1 um Rasterlinie nicht zu überdecken
-		var x1 = xGrid * this.getFieldSize() + 2 * this.getGridWidth();
-		var y1 = yGrid * this.getFieldSize() + 2 * this.getGridWidth();
+			getFieldSize : function() {
+				return (this.getImgSize() - 2 * this.getGridWidth())
+						/ this.getGridSize();
+			},
 
-		return new Array(x1, y1);
+			createGrid : function() {
+				var ctx = this.getCanvas().getContext("2d");
 
-	},
+				ctx.globalAlpha = 1;
+				ctx.fillStyle = "#000000";
 
-	getFieldSize : function() {
-		return (this.getImgSize() - 2 * this.getGridWidth())
-				/ this.getGridSize();
-	},
+				// rand
+				ctx.fillRect(0, 0, this.getGridWidth(), this.getImgSize());
+				ctx.fillRect(0, 0, this.getImgSize(), this.getGridWidth());
+				ctx.fillRect(this.getImgSize() - this.getGridWidth(), 0, this
+						.getGridWidth(), this.getImgSize());
+				ctx.fillRect(0, this.getImgSize() - this.getGridWidth(), this
+						.getImgSize(), this.getGridWidth());
 
-	createGrid : function() {
+				// innengatter
+				for (var i = 1; i < this.getGridSize(); i++) {
+					ctx.fillRect(this.getFieldSize() * i + this.getGridWidth(),
+							0, this.getGridWidth(), this.getImgSize());
+					ctx.fillRect(0, this.getFieldSize() * i
+							+ this.getGridWidth(), this.getImgSize(), this
+							.getGridWidth());
+				}
+			},
 
-		var ctx = this.getCanvas().getContext("2d");
+			// mark field by position parameters
+			markField : function(x, y) {
 
-		ctx.globalAlpha = 1;
-		ctx.fillStyle = "#000000";
+				var ctx = this.getCanvas().getContext("2d");
+				var koord = this.getFieldKoord(x, y);
 
-		// rand
-		ctx.fillRect(0, 0, this.getGridWidth(), this.getImgSize());
-		ctx.fillRect(0, 0, this.getImgSize(), this.getGridWidth());
-		ctx.fillRect(this.getImgSize() - this.getGridWidth(), 0, this
-				.getGridWidth(), this.getImgSize());
-		ctx.fillRect(0, this.getImgSize() - this.getGridWidth(), this
-				.getImgSize(), this.getGridWidth());
+				ctx.globalAlpha = 0.5;
+				ctx.fillStyle = "#C0FFEE";
+				ctx.fillRect(koord[0], koord[1], this.getFieldSize()
+						- this.getGridWidth(), this.getFieldSize()
+						- this.getGridWidth());
+			},
 
-		// innengatter
-		for (var i = 1; i < this.getGridSize(); i++) {
-			ctx.fillRect(this.getFieldSize() * i + this.getGridWidth(), 0, this
-					.getGridWidth(), this.getImgSize());
-			ctx.fillRect(0, this.getFieldSize() * i + this.getGridWidth(), this
-					.getImgSize(), this.getGridWidth());
-		}
-	},
+			onclick : function(event) {
+				var container = this.parentContainer;
+				var x = event.clientX;
+				var y = event.clientY;
+				var position = container.getFieldPosition(x, y);
 
-	markField : function(x, y) {
+				// eigenes indexof
+				var index = -1;
+				var fields = container.getChosenFields();
+				for (var i = 0; i < fields.length; i++) {
+					if (fields[i][0] == position[0]
+							&& fields[i][1] == position[1]) {
+						index = i;
+						break;
+					}
+				}
 
-		var ctx = this.getCanvas().getContext("2d");
+				var changed = false;
+				if (index > -1) {
+					container.getChosenFields().splice(index, 1);
+					changed = true;
+				} else if (container.getMaxChosenFields() > container
+						.getChosenFields().length) {
+					container.getChosenFields().push(position);
+					changed = true;
+				}
 
-		ctx.globalAlpha = 0.5;
-		ctx.fillStyle = "#C0FFEE";
-		ctx.fillRect(x, y, this.getFieldSize() - this.getGridWidth(), this
-				.getFieldSize()
-				- this.getGridWidth());
-	},
+				if (changed) {
+					container.clearAll();
+					container.createGrid();
 
-	onclick : function(event) {
+					container.getChosenFields().forEach(function(entry) {
+						container.markField(entry[0], entry[1]);
+					});
+				}
 
-		var info = {};
+			},
 
-		info.thiz = this;
-		info.event = event;
+			showGridElement : function(x1, y1, x2, y2) {
+				var ctx = document.getElementById("canvasWrapper").getContext(
+						"2d");
+				ctx.fillStyle = "#C0FFEE";
+				ctx.fillRect(x1, y1, x2, y2);
+			},
 
-		var canvas = document.getElementById("canvasWrapper");
-		var ctx = canvas.getContext("2d");
-		var container = this.parentContainer;
-
-		container.clearAll();
-		container.createGrid();
-
-		var x = event.clientX;
-		var y = event.clientY;
-
-		info.absolutKoord = {
-			xKoord : x,
-			yKoord : y
-		};
-
-		var gridKorrd = container.getFieldKoord(x, y);
-
-		info.gridKoord = {
-			xKoord : gridKorrd[0],
-			yKoord : gridKorrd[1]
-		};
-
-		container.markField(gridKorrd[0], gridKorrd[1]);
-
-		// infoausgabe über clickevent
-		console.log(info);
-
-	},
-
-	showGridElement : function(x1, y1, x2, y2) {
-		var ctx = document.getElementById("canvasWrapper").getContext("2d");
-		ctx.fillStyle = "#C0FFEE";
-		ctx.fillRect(x1, y1, x2, y2);
-	},
-
-});
+		});
