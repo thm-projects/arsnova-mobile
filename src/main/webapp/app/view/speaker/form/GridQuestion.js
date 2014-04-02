6018d4379544954695b534358acd10f28e2ae033
Artjom <siebertartjom@googlemail.com>
2014-04-02 19:59:44 +0200

    change position of buttons in edit mode

--- src/main/webapp/app/view/speaker/form/GridQuestion.js
+++ src/main/webapp/app/view/speaker/form/GridQuestion.js
@@ -30,7 +30,7 @@ Ext.define('ARSnova.view.speaker.form.GridQuestion', {
 	imageArea 		 : null,		// contains all image relevant items
 	grid 			 : null,		// encapsulated canvas element
 	imageCnt		 : null,		// image manipulation options
-	imageSettings 	 : null,		// the image settings (offset, zoom,...)
+	imageSettings 	 : null,			// the image settings (offset, zoom,...)
 	uploadView 		 : null,		// view containing the upload options
 	answers 		 : null,
 
@@ -44,7 +44,7 @@ Ext.define('ARSnova.view.speaker.form.GridQuestion', {
 
 		this.grid = Ext.create('ARSnova.view.components.GridContainer', {
 			docked 	: 'top',
-			id 		: 'gridContainer'
+			id 	: 'gridContainer'
 		});
 		
 		this.InfoButton = Ext.create('Ext.Panel',{
@@ -61,59 +61,72 @@ Ext.define('ARSnova.view.speaker.form.GridQuestion', {
 
 		this.imageArea = Ext.create('Ext.Panel', {
 			id : 'imageArea',
-			layout : 'hbox',
+			layout :{
+				type: 'vbox',
+                align: 'center',
+                pack: 'center'
+			}, 
 			items : [ 
-			    this.grid, 
-			    {
-				xtype: 'button',
-			    	iconCls : 'delete',
-			    	iconMask : true,
-			    	docked : 'right',
-			    	handler : function(){ me.resetView(); }
-				},
-			    {
-			    	xtype : 'button',
-				id : 'infobutton',
-			    	iconCls : 'info',
-			    	iconMask : true,
-			    	docked : 'right',
-				scope: this,
-				handler : function(){ 
-			    		this.onInfoButton(this);
-			    	}
-			    },
-			    {
-			    	xtype : 'button',
-			    	iconCls : 'reply',
-			    	iconMask : true,
-			    	docked : 'right',
-			    	handler : function(){ me.grid.toggleBorderColor(); }
-			    },
-			    {
-					xtype: 'button',
-			    	iconCls : 'arrow_left',
-			    	iconMask : true,
-			    	docked : 'right',
-					handler: function() { me.grid.moveLeft(); }
-				}, {
-				xtype: 'button',
-			    	iconCls : 'arrow_up',
-			    	iconMask : true,
-			    	docked : 'right',
-					handler: function() { me.grid.moveUp(); }
-				}, {
-					xtype: 'button',
-			    	iconCls : 'arrow_down',
-			    	iconMask : true,
-			    	docked : 'right',
-					handler: function() { me.grid.moveDown(); }
-				}, {
-					xtype: 'button',
-			    	iconCls : 'arrow_right',
-			    	iconMask : true,
-			    	docked : 'right',
-					handler: function() { me.grid.moveRight(); }
-				}],
+				this.grid,
+				{
+					xtype: 'panel',
+					layout:{
+						type: 'hbox',
+						align: 'center',
+						pack: 'center'
+					},
+					items : [
+						{
+							xtype : 'button',
+							id : 'infobutton',
+							iconCls : 'info',
+							iconMask : true,
+							//docked : 'right',
+							scope: this,
+							handler : function(){ 
+								this.onInfoButton(this);
+							}
+						},
+						{
+							xtype : 'button',
+							iconCls : 'reply',
+							iconMask : true,
+							//docked : 'right',
+							handler : function(){ me.grid.toggleBorderColor(); }
+						},
+						{
+							xtype: 'button',
+							iconCls : 'arrow_left',
+							iconMask : true,
+							//docked : 'right',
+							handler: function() { me.grid.moveLeft(); }
+						}, {
+							xtype: 'button',
+							iconCls : 'arrow_up',
+							iconMask : true,
+							//docked : 'right',
+							handler: function() { me.grid.moveUp(); }
+						}, {
+							xtype: 'button',
+							iconCls : 'arrow_down',
+							iconMask : true,
+							//docked : 'right',
+							handler: function() { me.grid.moveDown(); }
+						}, {
+							xtype: 'button',
+							iconCls : 'arrow_right',
+							iconMask : true,
+							//docked : 'right',
+							handler: function() { me.grid.moveRight(); }
+						},{
+							xtype: 'button',
+							iconCls : 'delete',
+							iconMask : true,
+							handler : function(){ me.resetView(); }
+						}
+			       ]
+			   }
+			],
 			hidden : true
 		});
 
