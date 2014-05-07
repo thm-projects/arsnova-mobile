f5b0ba7430e1ed1f97983636371b1a2cfe0e6f5c
Christoph Thelen <christoph.thelen@mni.thm.de>
2014-05-07 15:54:57 +0200

    Fixed double encoding of entities

--- src/main/webapp/app/view/Question.js
+++ src/main/webapp/app/view/Question.js
@@ -202,10 +202,10 @@ Ext.define('ARSnova.view.Question', {
 			}
 		};
 
-		//Setup question title and text to disply in the same field
-		var questionString = Ext.util.Format.htmlEncode(this.questionObj.subject)
+		//Setup question title and text to disply in the same field; markdown handles HTML encoding
+		var questionString = this.questionObj.subject
                        + '\n\n' // inserts one blank line between subject and text
-                       + Ext.util.Format.htmlEncode(this.questionObj.text);
+                       + this.questionObj.text;
 
 		//Create standard panel with framework support
 		var questionPanel = Ext.create('ARSnova.view.MathJaxMarkDownPanel');
