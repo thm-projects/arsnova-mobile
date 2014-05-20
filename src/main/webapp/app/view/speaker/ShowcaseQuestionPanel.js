0555bb7adef2004a9722bbbd579155935c7658cf
Christoph Thelen <christoph.thelen@mni.thm.de>
2014-05-20 15:01:17 +0200

    Refactored a toolbar with support for displaying question info

--- src/main/webapp/app/view/speaker/ShowcaseQuestionPanel.js
+++ src/main/webapp/app/view/speaker/ShowcaseQuestionPanel.js
@@ -17,154 +17,79 @@
  +--------------------------------------------------------------------------*/
 Ext.define('ARSnova.view.speaker.ShowcaseQuestionPanel', {
 	extend: 'Ext.Carousel',
-	
-	requires: ['ARSnova.view.Question',
-	           'ARSnova.view.speaker.QuestionStatisticChart'
+
+	requires: [
+    'ARSnova.view.Question',
+	  'ARSnova.view.speaker.QuestionStatisticChart',
+    'ARSnova.view.components.QuestionToolbar'
 	],
-	
+
 	config: {
 		fullscreen: true,
 		title	: Messages.QUESTIONS,
 		iconCls	: 'tabBarIconQuestion',
-		
+
 		controller: null
 	},
-	
-	/* toolbar items */
-	toolbar		: null,
-	backButton	: null,
-	
-	questionCounter: 0,
-	
+
 	initialize: function() {
 		this.callParent(arguments);
-		
+
 		this.on('activeitemchange', function(panel, newCard, oldCard) {
-			var label = Ext.bind(function(longv, shortv) {
-				var screenWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-				return (screenWidth > 320 || this.backButton.isHidden()) ? longv : shortv;
-				
-			}, this);
-			var questionType = newCard.questionObj.questionType;
-			var title = '';
-			
-			if (questionType === 'abcd') {
-				title = label(Messages.QUESTION_SINGLE_CHOICE, Messages.QUESTION_SINGLE_CHOICE_SHORT);
-			} else if (questionType === 'freetext') {
-				title = label(Messages.QUESTION_FREETEXT, Messages.QUESTION_FREETEXT_SHORT);
-			} else if (questionType === 'mc') {
-				title = label(Messages.QUESTION_MC, Messages.QUESTION_MC_SHORT);
-			} else if (questionType === 'vote') {
-				title = label(Messages.QUESTION_RATING, Messages.QUESTION_RATING_SHORT);
-			} else if (questionType === 'yesno') {
-				title = label(Messages.QUESTION_YESNO, Messages.QUESTION_YESNO);
-			} else if (questionType === 'school') {
-				title = label(Messages.QUESTION_GRADE, Messages.QUESTION_GRADE_SHORT);
-			} else if (questionType === 'flashcard') {
-				title = label(Messages.FLASHCARD, Messages.FLASHCARD);
-			} else if(questionType == 'grid') {
-				title =  label(Messages.QUESTION_GRID, Messages.GRID);
-			}
-			
-			this.toolbar.setTitle(title);
-			
-			//update question counter in toolbar
-			var counterEl = panel.questionCounter;
-			var counter = counterEl.getHtml().split("/");
+			this.toolbar.setQuestionTitle(newCard.questionObj.questionType);
+			this.toolbar.incrementQuestionCounter(panel.activeIndex);
 
-			counter[0] = panel.activeIndex + 1;
-			counterEl.setHtml(counter.join("/"));
+			newCard.fireEvent('preparestatisticsbutton', this.toolbar.statisticsButton);
+		}, this);
 
-			newCard.fireEvent('preparestatisticsbutton', panel.statisticButton);
-		});
-		
-		this.questionCounter = Ext.create('Ext.Container', {
-			cls: "x-toolbar-title alignRight counterText",
-			html: '0/0'
-		});
-		
-		this.statisticButton = Ext.create('Ext.Button', {
-			text	: ' ',
-			cls		: 'statisticIconSmall',
-			handler	: function() {
-				var sTP = ARSnova.app.mainTabPanel.tabPanel.speakerTabPanel;
-				sTP.questionStatisticChart = Ext.create('ARSnova.view.speaker.QuestionStatisticChart', {
-					question: ARSnova.app.mainTabPanel.tabPanel.speakerTabPanel._activeItem._activeItem.questionObj, 
-					lastPanel: this
-				});
-				ARSnova.app.mainTabPanel.animateActiveItem(sTP.questionStatisticChart, 'slide');
-			}
-		});
-		
-		this.backButton = Ext.create('Ext.Button', {
-			ui		: 'back',
-			text	: Messages.BACK,
-			scope	: this,
-			handler	: function() {
-				var sTP = ARSnova.app.mainTabPanel.tabPanel.speakerTabPanel;
-				sTP.animateActiveItem(sTP.audienceQuestionPanel, {
-					type		: 'slide',
-					direction	: 'right',
-					duration	: 700,
-					scope		: this,
-		    		listeners: { animationend: function() { 
-		    			this.hide();
-		    		}, scope: this }
-				});
-			}
-		});
-		
-		this.toolbar = Ext.create('Ext.Toolbar', {
-			title: Messages.QUESTION,
-			docked: 'top',
-			ui: 'light',
-			items: [
-			    this.backButton,
-		        { xtype: 'spacer' },
-		        this.questionCounter,
-		        this.statisticButton
-	        ]
-		});
+		this.toolbar = Ext.create('ARSnova.view.components.QuestionToolbar', {
+      backButtonHandler: function(animation) {
+        var sTP = ARSnova.app.mainTabPanel.tabPanel.speakerTabPanel;
+        sTP.animateActiveItem(sTP.audienceQuestionPanel, animation);
+      },
+      statisticsButtonHandler: function() {
+        var sTP = ARSnova.app.mainTabPanel.tabPanel.speakerTabPanel;
+        sTP.questionStatisticChart = Ext.create('ARSnova.view.speaker.QuestionStatisticChart', {
+          question: ARSnova.app.mainTabPanel.tabPanel.speakerTabPanel._activeItem._activeItem.questionObj,
+          lastPanel: this
+        });
+        ARSnova.app.mainTabPanel.animateActiveItem(sTP.questionStatisticChart, 'slide');
+      }
+    });
 
 		this.add([this.toolbar]);
-		
+
 		this.on('activate', this.beforeActivate, this, null, 'before');
 		this.on('activate', this.onActivate);
 		this.on('add', function(panel, component, index) {
 			component.doTypeset && component.doTypeset(panel);
 		});
 	},
-	
+
 	beforeActivate: function(){
-		this.removeAll(true);
+		this.removeAll();
 		this._indicator.show();
-		this.questionCounter.show();
 		this.toolbar.setTitle(Messages.QUESTION);
 	},
-	
+
 	onActivate: function(){
 		this.getAllSkillQuestions();
 	},
-	
+
 	getAllSkillQuestions: function() {
 		var hideIndicator = ARSnova.app.showLoadMask(Messages.LOAD_MASK_SEARCH_QUESTIONS);
-		
+
 		this.getController().getQuestions(localStorage.getItem("keyword"), {
 			success: function(response) {
 				var questions = Ext.decode(response.responseText);
 				var panel = ARSnova.app.mainTabPanel.tabPanel.speakerTabPanel.showcaseQuestionPanel;
-				
-				//update question counter in toolbar
-				var counterEl = panel.questionCounter;
-				var counter = counterEl.getHtml().split("/");
-				counter[0] = "1";
-				counter[1] = questions.length;
-				counterEl.setHtml(counter.join("/"));
-				
+
+				panel.toolbar.resetQuestionCounter(questions.length);
+
 				if (questions.length == 1){
 					panel._indicator.hide();
 				}
-				
+
 				var questionsArr = [];
 				var questionIds = [];
 				questions.forEach(function(question){
@@ -174,7 +99,7 @@ Ext.define('ARSnova.view.speaker.ShowcaseQuestionPanel', {
 				questionIds.forEach(function(questionId){
 					panel.addQuestion(questionsArr[questionId]);
 				});
-				
+
 				// bugfix (workaround): after removing all items from carousel the active index
 				// is set to -1. To fix that you have manually  set the activeItem on the first
 				// question.
@@ -188,24 +113,26 @@ Ext.define('ARSnova.view.speaker.ShowcaseQuestionPanel', {
 			}
 		});
 	},
-	
-	addQuestion: function(question){
+
+	addQuestion: function(question) {
+    var question;
 		if (question.questionType === 'freetext') {
-			this.add(Ext.create('ARSnova.view.FreetextQuestion', {
-				questionObj: question, 
+			question = Ext.create('ARSnova.view.FreetextQuestion', {
+				questionObj: question,
 				viewOnly: true
-			}));
+			});
 		} else {
-			this.add(Ext.create('ARSnova.view.Question', {
-				questionObj: question, 
+			question = Ext.create('ARSnova.view.Question', {
+				questionObj: question,
 				viewOnly: true
-			}));
+			});
 		}
+    this.add(question);
 	},
-	
+
 	checkFirstQuestion: function() {
 		var firstQuestionView = this.items.items[0];
-		
-		firstQuestionView.fireEvent('preparestatisticsbutton', this.statisticButton);
+
+		firstQuestionView.fireEvent('preparestatisticsbutton', this.toolbar.statisticsButton);
 	}
 });
