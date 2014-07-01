012e44b6750edd3b8a457d1070e0a75cc0663d2b
Christoph Thelen <christoph.thelen@mni.thm.de>
2014-07-01 11:25:50 +0200

    Class rewritten with retina image support

--- src/main/webapp/app/view/MatrixButton.js
+++ src/main/webapp/app/view/MatrixButton.js
@@ -15,33 +15,50 @@
  along with this program; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  +--------------------------------------------------------------------------*/
-
-
 Ext.define('ARSnova.view.MatrixButton', {
 	extend: 'Ext.Button',
 	alias: 'x-matrixbutton',
 	xtype: 'matrixbutton',
-	
+
 	requires: ['Ext.Img'],
-	
+
 	config: {
 		image: '',
+		text: '',
 		cls: 'noBackground noBorder matrixButton',
-		html: [
-		          '<span class="iconBtn"></span><span class="gravure buttonText" style="display:block"></span>'
-		         ],
-	     listeners: {
-	    	    initialize: function (element, options) {
-	    	    	var parent = Ext.get(element.id);
-	    	    	var buttonText = parent.select(".buttonText").elements;
-	    	    	buttonText[0].innerHTML = Ext.getCmp(element.id).get("text");
-	    	      
-		    	    Ext.create('Ext.Img', {
-	    	    		src: "resources/images/" + Ext.getCmp(element.id).get("image") + ".png",
-	    	    	    renderTo: parent.select(".iconBtn").elements[0],
-	    	    	    cls: "iconBtnImg"
-	    	    	});    
-	    	    }
-	     }
+		html: ['<span class="iconBtn"></span><span class="gravure buttonText" style="display:block"></span>'],
+		listeners: {
+			initialize: function (element, options) {
+				var parent = Ext.get(element.id);
+				var buttonText = parent.select(".buttonText").elements;
+				var retina = window.devicePixelRatio >= 2;
+
+				var imagefile = "resources/images/" + this.getImage() + ".png";
+				var imagefile2x = "resources/images/" + this.getImage() + "@2x.png";
+				var promise = new RSVP.Promise();
+				if (retina) {
+					var img = new Image();
+					img.onload = function imageExists() {
+						promise.resolve(imagefile2x);
+					};
+					img.onerror = function imageNotFound() {
+						promise.resolve(imagefile);
+					};
+					img.src = imagefile2x;
+				} else {
+					promise.resolve(imagefile);
+				}
+
+				buttonText[0].innerHTML = this.getText();
+
+				promise.then(function(theImage) {
+					Ext.create('Ext.Img', {
+						src: theImage,
+						renderTo: parent.select(".iconBtn").elements[0],
+						cls: "iconBtnImg"
+					});
+				});
+			}
+		}
 	}
 });
