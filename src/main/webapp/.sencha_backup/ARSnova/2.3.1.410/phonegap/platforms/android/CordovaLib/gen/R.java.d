bd25af9b3abcbbbedeec21ff28e652c5c4ffc1f0
Daniel Gerhardt <code@dgerhardt.net>
2014-09-08 12:41:17 +0200

    Remove Sencha backup dir and add it to .gitignore

--- src/main/webapp/.sencha_backup/ARSnova/2.3.1.410/phonegap/platforms/android/CordovaLib/gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /Users/andy/Projekte/arsnova/arsnova-mobile/src/main/webapp/phonegap/platforms/android/CordovaLib/bin/AndroidManifest.xml \
--- src/main/webapp/.sencha_backup/ARSnova/2.3.1.410/phonegap/platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/andy/Projekte/arsnova/arsnova-mobile/src/main/webapp/phonegap/platforms/android/bin/AndroidManifest.xml : \
-/Users/andy/Projekte/arsnova/arsnova-mobile/src/main/webapp/phonegap/platforms/android/AndroidManifest.xml \
