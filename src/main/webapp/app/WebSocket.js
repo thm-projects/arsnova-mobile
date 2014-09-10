thm-projects/arsnova-mobile

commit ab9453920ff8dcb5f7e37bd96ef4ac9989dd9857

Fix case for browsers's WebSocket constructor

src/main/webapp/app/WebSocket.js
@@ -42,7 +42,7 @@ Ext.define('ARSnova.WebSocket', {
 				/* Workaround: unfortunately some browsers pretend to support
 				 * WS protocol although they do not */
 				try {
-					var ws = new Websocket("wss:" + window.location.hostname + ":10443");
+					var ws = new WebSocket("wss:" + window.location.hostname + ":10443");
 					ws.close(-1);
 				} catch (e) {
 					hasWs = true;

commit c38c0978ca81b26095adfe4a68d70ddf70a51ae2

Fix test for WebSocket support
This fixes a regression introduced by ab94539.

src/main/webapp/app/WebSocket.js
@@ -43,7 +43,8 @@ Ext.define('ARSnova.WebSocket', {
 				/* Workaround: unfortunately some browsers pretend to support
 				 * WS protocol although they do not */
 				try {
-					var ws = new WebSocket("wss:" + window.location.hostname + ":10443");
+					var wsTestUrl = socketUrl.replace(/^http/, "ws") + "/socket.io/1/";
+					var ws = new WebSocket(wsTestUrl);
 					ws.close(-1);
 				} catch (e) {
 					hasWs = true;