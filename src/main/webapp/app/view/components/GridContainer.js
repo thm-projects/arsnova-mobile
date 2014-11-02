ce4d77ea645e75785edeb360d98ae21d76e9a9be
Felix Schmidt <felix.schmidt@mni.thm.de>
2014-11-02 22:16:01 +0100

    Fixed zoom in grid templates.

--- src/main/webapp/app/view/components/GridContainer.js
+++ src/main/webapp/app/view/components/GridContainer.js
@@ -875,6 +875,9 @@ Ext.define('ARSnova.view.components.GridContainer', {
 						if (typeof(config.cvBackgroundColor) != "undefined") me.setCvBackgroundColor(config.cvBackgroundColor);
 						if (typeof(config.cvIsColored) != "undefined") me.setCvIsColored(config.cvIsColored);
 						
+						me.initZoom();
+						me.initGridZoom();
+						
 						me.redraw();
 					}, function() {
 						console.log("Could not set config. Error while loading image: '" + url + "'.");
