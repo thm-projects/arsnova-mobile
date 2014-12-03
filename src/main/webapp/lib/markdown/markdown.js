8a7b3b07616a50fbff126e329c65c1777bacd657
Christoph Thelen <christoph.thelen@mni.thm.de>
2014-12-03 13:40:42 +0100

    Revert "Fixed #13371"
    This reverts commit 9bbd84126e80c63f0236b36c6d15eb039614f6d6.

--- src/main/webapp/lib/markdown/markdown.js
+++ src/main/webapp/lib/markdown/markdown.js
@@ -796,7 +796,7 @@
       //
       lists: (function( ) {
         // Use a closure to hide a few variables.
-        var any_list = "[*+-]|\\d+§",
+        var any_list = "[*+-]|\\d+\\.",
             bullet_list = /[*+-]/,
             // Capture leading indent as it matters for determining nested lists.
             is_list_re = new RegExp( "^( {0,3})(" + any_list + ")[ \t]+" ),
