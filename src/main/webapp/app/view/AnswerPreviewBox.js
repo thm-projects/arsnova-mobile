8a9e1c9d30945ab3344dd790287e12101db38ee6
agrt56 <andreas.gaertner@mni.thm.de>
2014-12-03 14:06:24 +0100

    Revised and updated AnswerPreviewBox. -complete code overhaule ,-removed Carousel, -added list, -added statisticChart for expandingAnswers

--- src/main/webapp/app/view/AnswerPreviewBox.js
+++ src/main/webapp/app/view/AnswerPreviewBox.js
@@ -18,83 +18,97 @@
  */
 
 Ext.define('ARSnova.view.AnswerPreviewBox', {
-	xtype: 'answerPreview',
-	ui: 'normal',
-
-	showPreview: function (answers, questionType) {
-		this.answers = answers;
-
-		// answer preview message box
-		this.previewBox = Ext.create('Ext.MessageBox', {
-			scrollable: true,
-			layout: 'vbox',
-			style: 'font-size: 110%; border-color: black; maxHeight: 600px; maxWidth: 1000px; width: 80%; height: 80%',
-			scope: this
+	extend: 'Ext.MessageBox',
+	
+	config: {
+		scrollable: true,
+		hideOnMaskTap: true,
+		layout: 'fit'
+	},
+	
+	initialize: function(args) {
+		this.callParent(args);
+		
+		this.setStyle({
+			'font-size': '110%',
+			'border-color': 'black',
+			'maxHeight': '600px',
+			'maxWidth': '1000px',
+			'height': '80%',
+			'width': '80%'
 		});
 		
 		this.toolbar = Ext.create('Ext.Toolbar', {
 			title: Messages.ANSWER_PREVIEW_DIALOGBOX_TITLE,
 			docked: 'top',
 			ui: 'light',
-			/*items: [
+			items: [
 			    {xtype: 'spacer'},
-			    Ext.create('Ext.Button', {
+			    this.statisticButton = Ext.create('Ext.Button', {
 			    	iconCls: 'icon-chart',
 			    	style: 'padding: 0 0.4em',
 			    	handler: this.statisticsButtonHandler,
 			    	scope: this
 			    })
-			]*/
+			]
 		});
 		
-		// question preview confirm button
-		var confirmButton = Ext.create('Ext.Button', {
-			text: Messages.QUESTION_PREVIEW_DIALOGBOX_BUTTON_TITLE,
-			id: 'confirmButton',
-			xtype: 'button',
-			ui: 'confirm',
-			//cls: 'previewButtonOK',
-			style: 'width: 80%; maxWidth: 250px; margin-top: 10px;',
-			scope: this,
-			handler: function () {
-				this.previewBox.destroy();
-			}
+		this.confirmButton = Ext.create('Ext.Container', {
+			layout: {
+				pack: 'center',
+				type: 'hbox'
+			},
+			items: [
+				Ext.create('Ext.Button', {
+					text: Messages.QUESTION_PREVIEW_DIALOGBOX_BUTTON_TITLE,
+					id: 'confirmButton',
+					ui: 'confirm',
+					style: 'width: 80%; maxWidth: 250px; margin-top: 10px;',
+					scope: this,
+					handler: function () {
+						this.hide();
+					}
+				})
+			]
 		});
 		
-		var answerStore = Ext.create('Ext.data.Store', {model: 'ARSnova.model.Answer'});
-		answerStore.add(answers);
-		answerStore.each(function (item) {
-			if (ARSnova.app.globalConfig.parseAnswerOptionFormatting) {
-				var md = Ext.create('ARSnova.view.MathJaxMarkDownPanel');
-				md.setContent(item.get('text'), true, true, function (html) {
-					item.set('formattedText', html.getHtml());
-					md.destroy();
-				});
-			} else {
-				item.set('formattedText', Ext.util.Format.htmlEncode(item.get('text')));
-			}
+		// answer preview box content panel
+		this.mainPanel = Ext.create('Ext.Container', {
+			id: 'mainPanel',
+			layout: 'vbox',
+			style: 'margin-bottom: 10px;',
+			styleHtmlContent: true,
+			fullscreen: true
 		});
+		
+		this.on('hide', this.destroy);
+	},
+	
+	showPreview: function (answers, questionType) {
+		this.answers = answers;
 		
 		if(questionType === 'flashcard') {
 			this.answerList = Ext.create('ARSnova.view.MathJaxMarkDownPanel', {
-		    	xtype: 'mathJaxMarkDownPanel',
-		    	flex: 1,
-		    	style: 'margin-left: 0px; margin-right: 0px;word-wrap: break-word;'
+		    	style: 'margin-left: 0px; margin-right: 0px; word-wrap: break-word;',
+		    	flex: 1
 			});
 			
+			this.statisticButton.setHidden(true);
 			this.answerList.setContent(answers[0].text, true, true);
 		}
 		
-		else {	
+		else {
 			this.answerList = Ext.create('Ext.List', {
-				store: answerStore,
+				store: Ext.create('Ext.data.Store', {
+					model: 'ARSnova.model.Answer'
+				}),
 	
 				cls: 'roundedBox',
 				variableHeights: true,
 				scrollable: {disabled: true},
 	
-				itemCls: 'arsnova-mathdown x-html',
 				itemHeight: '32px',
+				itemCls: 'arsnova-mathdown x-html',
 				itemTpl: new Ext.XTemplate(
 					"<tpl if='this.isFormattedStringEmpty(formattedText) === true'>",
 						"&nbsp;",
@@ -103,25 +117,13 @@ Ext.define('ARSnova.view.AnswerPreviewBox', {
 					"</tpl>",
 					{
 						isFormattedStringEmpty: function(formattedString) {						
-							if(formattedString === "") {
-								return true;
-							}
-							
-							return false;
+							if(formattedString === "") { return true; }
+							else { return false; }
 						}
 					}
 				),
 				listeners: {
 					scope: this,
-					
-					/**
-					 * The following events are used to get the computed height of
-					 * all list items and finally to set this value to the list
-					 * DataView. In order to ensure correct rendering it is also
-					 * necessary to get the properties "padding-top" and
-					 * "padding-bottom" and add them to the height of the list
-					 * DataView.
-					 */
 					painted: function (list, eOpts) {
 						this.answerList.fireEvent("resizeList", list);
 					},
@@ -136,34 +138,32 @@ Ext.define('ARSnova.view.AnswerPreviewBox', {
 					}
 				}
 			});
-		}
 			
-		// answer preview box content panel
-		this.mainPanel = Ext.create('Ext.Container', {
-			id: 'mainPanel',
-			flex: '1',
-			xtype: 'container',
-			style: 'width: 100%; height: 100%; margin-bottom: 10px;',
-			layout: 'vbox',
-			fullscreen: false,
-			items: [
-				this.answerList,
-				{
-					id: 'buttonLayout',
-					xtype: 'container',
-					layout: {
-						pack: 'center',
-						type: 'hbox'
-					},
-					items: [confirmButton]
+			this.answerList.getStore().add(answers);
+			this.answerList.getStore().each(function (item) {
+				if (ARSnova.app.globalConfig.parseAnswerOptionFormatting) {
+					var md = Ext.create('ARSnova.view.MathJaxMarkDownPanel');
+					md.setContent(item.get('text'), true, true, function (html) {
+						item.set('formattedText', html.getHtml());
+						md.destroy();
+					});
+				} else {
+					item.set('formattedText', Ext.util.Format.htmlEncode(item.get('text')));
 				}
-			]
-		});
-		this.mainPanel.setStyleHtmlContent(true);
+			});
+		}		
+
+		this.mainPanel.add([
+			this.answerList,
+			this.confirmButton
+		]);
 		
-		this.previewBox.add(this.toolbar);
-		this.previewBox.add(this.mainPanel);
-		this.previewBox.show();
+		this.add([
+			this.toolbar,
+			this.mainPanel
+		]);
+
+		this.show();
 	},
 	
 	statisticsButtonHandler: function () {
@@ -171,12 +171,16 @@ Ext.define('ARSnova.view.AnswerPreviewBox', {
 		questionObj.possibleAnswers = this.answers;
 		
 		this.questionStatisticChart = Ext.create('ARSnova.view.AnswerPreviewStatisticChart', {
-			layout: 'fit',
-			answerObj: questionObj
+			question: questionObj,
+			lastPanel: this
 		});
+			
+		this.remove(this.toolbar, false);
+		this.remove(this.mainPanel, false);
+		this.setScrollable(false);
 		
-		this.previewBox.remove(this.toolbar);
-		this.previewBox.remove(this.mainPanel);
-		this.previewBox.add(this.questionStatisticChart);
+		this.add([	
+	    	this.questionStatisticChart
+		]);
 	}
 });
