a006f305b70fc04d10965a9122f1762b9b066b6f
Daniel Vogel <daniel.vogel@mni.thm.de>
2014-12-13 13:58:13 +0100

    Implemented question import

--- src/main/webapp/app/proxy/RestProxy.js
+++ src/main/webapp/app/proxy/RestProxy.js
@@ -381,6 +381,10 @@ Ext.define('ARSnova.proxy.RestProxy', {
 	},
 
 	saveSkillQuestion: function (question, callbacks) {
+		console.log("saveSkillQuestion - question");
+		console.log(question);
+		console.log("question.getData()");
+		console.log(question.getData());
 		this.arsjax.request({
 			url: "session/" + question.get('sessionKeyword') + "/question",
 			method: "POST",
