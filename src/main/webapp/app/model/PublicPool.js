4ec92104e3cd61e19d7b3d3269c0ffcd09e19b61
asbr90 <siebertartjom@googlemail.com>
2015-01-07 00:35:08 +0100

    created new model: PublicPool.js for validation

---
+++ src/main/webapp/app/model/PublicPool.js
@@ -0,0 +1,68 @@
+/*
+ * This file is part of ARSnova Mobile.
+ * Copyright (C) 2011-2012 Christian Thomas Weber
+ * Copyright (C) 2012-2014 The ARSnova Team
+ *
+ * ARSnova Mobile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ARSnova Mobile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ARSnova Mobile.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+Ext.define('ARSnova.model.PublicPool', {
+	extend: 'Ext.data.Model',
+	config:{
+	 fields: [
+	          {
+	        	  name: 'name'
+	          },
+	          {
+	        	  name: 'hs'
+	          },
+	          {
+	        	  name: 'logo'
+	          },
+	          {
+	        	  name: 'subject'
+	          },
+	          {
+	        	  name:	'licence'
+	          },
+	          {
+	        	 name:	'email' 
+	          }
+	         ],
+	 validations: [
+	              {
+	            	  type: 'presence',
+	                  field: 'name',
+	                  message: 'Not enter a name'
+	              },
+	              {
+	            	  type: 'presence',
+	                  field: 'hs'
+	              },
+	              {
+	            	  type: 'presence',
+	                  field: 'subject'
+	              },	      
+	              {
+	            	  type: 'presence',
+	                  field: 'licence'
+	              },
+	              {
+	            	  type: 'email',
+	                  field: 'email',
+	                  message: 'Email format is invalid'
+	              }
+	            ]
+	}
+});
