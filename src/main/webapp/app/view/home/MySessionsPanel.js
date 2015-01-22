341681d070e4473b72f647437c2915f69abd3630
asbr90 <siebertartjom@googlemail.com>
2015-01-22 15:53:50 +0100

    disappeared when you tap anywhere on the screen

--- src/main/webapp/app/view/home/MySessionsPanel.js
+++ src/main/webapp/app/view/home/MySessionsPanel.js
@@ -268,6 +268,7 @@ Ext.define('ARSnova.view.home.MySessionsPanel', {
 								title: Messages.EXPORT_SELECTED_SESSIONS_TITLE,
 								message: Messages.EXPORT_SELECTED_SESSIONS_MSG,
 								buttons: msgBox.YESNO,
+								hideOnMaskTap: true,
 								listeners:[ 
 								            {
 								                element: 'element',
