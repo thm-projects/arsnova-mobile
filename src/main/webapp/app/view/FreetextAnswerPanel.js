9e0262cf166c3b21636698621ff04a047fc19597
agrt56 <andreas.gaertner@mni.thm.de>
2015-01-22 19:14:46 +0100

    Added message to freetextAnswerPanel if no ansers were given or if only abstention answers were given. Task #14471

--- src/main/webapp/app/view/FreetextAnswerPanel.js
+++ src/main/webapp/app/view/FreetextAnswerPanel.js
@@ -78,6 +78,15 @@ Ext.define('ARSnova.view.FreetextAnswerPanel', {
 			ui: 'light',
 			items: [this.backButton]
 		});
+		
+		this.noAnswersLabel = Ext.create('Ext.form.FormPanel', {
+			scrollable: null,
+			hidden: true,
+			items: {
+				cls: 'gravure',
+				html: Messages.NO_ANSWERS
+			}
+		});
 
 		this.freetextAnswerList = Ext.create('Ext.List', {
 			activeCls: 'search-item-active',
@@ -132,7 +141,7 @@ Ext.define('ARSnova.view.FreetextAnswerPanel', {
 			width: '100%',
 			flex: 1,
 			scrollable: null,
-			items: [this.freetextAnswerList]
+			items: [this.noAnswersLabel, this.freetextAnswerList]
 		}]);
 
 		this.on('activate', function () {
@@ -151,34 +160,63 @@ Ext.define('ARSnova.view.FreetextAnswerPanel', {
 	},
 
 	checkFreetextAnswers: function () {
+		var me = this;
+		
 		ARSnova.app.questionModel.getAnsweredFreetextQuestions(localStorage.getItem("keyword"), this.questionObj._id, {
 			success: function (response) {
 				var responseObj = Ext.decode(response.responseText);
-				var listItems = responseObj.map(function (item) {
-					var v = item;
-					var date = new Date(v.timestamp);
-					return Ext.apply(item, {
-						formattedTime: Ext.Date.format(date, "H:i"),
-						groupDate: Ext.Date.format(date, "d.m.y")
+				var answerLabel = me.noAnswersLabel.getInnerItems()[0];
+
+				if(responseObj.length === 0) {
+					answerLabel.setHtml(Messages.NO_ANSWERS);
+					me.freetextAnswerList.hide();
+					me.noAnswersLabel.show();
+				} else {
+					me.freetextAnswerList.show();
+					var listItems = responseObj.map(function (item) {
+						var v = item;
+						var date = new Date(v.timestamp);
+						return Ext.apply(item, {
+							formattedTime: Ext.Date.format(date, "H:i"),
+							groupDate: Ext.Date.format(date, "d.m.y")
+						});
 					});
-				});
 
-				var self = ARSnova.app.mainTabPanel._activeItem;
-				var abstentions = listItems.filter(function (item) {
-					return item.abstention;
-				});
-				var answers = listItems.filter(function (item) {
-					return !item.abstention;
-				});
+					var abstentions = listItems.filter(function (item) {
+						return item.abstention;
+					});
+					var answers = listItems.filter(function (item) {
+						return !item.abstention;
+					});
 
-				self.freetextAnswerStore.removeAll();
-				self.freetextAnswerStore.add(answers);
-				self.freetextAnswerStore.sort([{
-					property: 'timestamp',
-					direction: 'DESC'
-				}]);
-				self.freetextAbstentions.setBadgeText(abstentions.length);
-				self.freetextAbstentions.setHidden(abstentions.length === 0);
+					me.freetextAnswerStore.removeAll();
+					me.freetextAnswerStore.add(answers);
+					me.freetextAnswerStore.sort([{
+						property: 'timestamp',
+						direction: 'DESC'
+					}]);
+					me.freetextAbstentions.setBadgeText(abstentions.length);
+					me.freetextAbstentions.setHidden(abstentions.length === 0);	
+					
+					if(abstentions.length === responseObj.length) {
+						var abCount = abstentions.length;
+						var abstentionText = abCount === 1 ? Messages.ABSTENTION : Messages.ABSTENTIONS;
+						
+						if(lang === "en") {
+							var verb = abCount === 1 ? 'is ' : 'are ';
+							abstentionText = verb + abCount + " " + abstentionText.toLowerCase();
+						} else {
+							abstentionText = abCount + " " + abstentionText;
+						}
+						
+						answerLabel.setHtml(Messages.ONLY_ABSTENTION_ANSWERS.replace(/###/, abstentionText));
+						me.freetextAnswerList.hide();
+						me.noAnswersLabel.show();
+					} else {
+						me.freetextAnswerList.show();
+						me.noAnswersLabel.hide();
+					}
+				}
 			},
 			failure: function () {
 				console.log('server-side error');
