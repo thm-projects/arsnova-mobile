70294b664594c99450d35ff9d3eb33907b3403f3
DHikade <info@dhika.de>
2015-03-13 14:39:06 +0100

    Change german labels of alphabetic-, timesort and delete changes buttons

--- src/main/webapp/app/internationalization.js
+++ src/main/webapp/app/internationalization.js
@@ -233,10 +233,10 @@
 				SORT_SAVE_AND_CONTINUE: "Speichern und fortfahren",
 				SORT_TRANSMISSION_ERROR: "Das Sortieren war leider nicht erfolgreich.",
 				SORT_QUESTIONS: "Fragen<br/>sortieren",
-				SORT_ALPHABET: "Alphabetisch<br/>sortieren",
-				SORT_TIME: "Zeitlich<br/>sortieren",
+				SORT_ALPHABET: "Alphabet<br/>Sortierung",
+				SORT_TIME: "Zeitliche<br/>Sortierung",
 				SORT_RANDOM: "Zufällig<br/>anordnen",
-				SORT_REVERT: "Rückgängig<br/>machen",
+				SORT_REVERT: "Änderung<br/>verwerfen",
 
 				/* user */
 				QUESTIONS_TO_STUDENTS: "Fragen ans Publikum",
@@ -576,7 +576,7 @@
 				IMP_ERROR: "Fehler bei Session-Import",
 				IMP_ERROR_FORMAT: "JSON Datei ist falsch formatiert.",
 				IMP_ERROR_SAVE: "Session konnte nicht gespeichert werden.",
-				IMP_ERROR_IMAGE: "Session konnte nicht gespeichert werden. Bild überschreitet maximale Größe."
+				IMP_ERROR_IMAGE: "Session konnte nicht gespeichert werden. Bild überschreitet maximale Größe.",
 
 				/* Modification */
 				FREETEXT_TEST: "Ich bin ein Test",
@@ -1125,7 +1125,7 @@
 				IMP_ERROR: "Error while importing sessions",
 				IMP_ERROR_FORMAT: "JSON file is not formated correctly",
 				IMP_ERROR_SAVE: "Could not save session to database.",
-				IMP_ERROR_IMAGE: "Could not save session, image exceeds maximal size."
+				IMP_ERROR_IMAGE: "Could not save session, image exceeds maximal size.",
 
 				/* Modification */
 				FREETEXT_TEST: "I am a Test",
