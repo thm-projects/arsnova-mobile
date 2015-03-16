a8144cd344917c856bb7f00efdd6b4bc2254202b
N. Justus <nicola.justus@mni.thm.de>
2015-03-16 17:49:24 +0100

    Added answerText and conditional template. CSS needs to get fixed. #15402

--- src/main/webapp/app/view/ImageAnswerPanel.js
+++ src/main/webapp/app/view/ImageAnswerPanel.js
@@ -32,7 +32,10 @@ Ext.define('ARSnova.view.ImageAnswerPanel', {
 
 		freetextAnswerStore: null
 	},
+		//flag for vertical or horizontal list
+	isVertical: false,
 	constructor: function (args) {
+		var me = this;
 		this.callParent(arguments);
 
 		this.questionObj = args.question;
@@ -143,17 +146,30 @@ Ext.define('ARSnova.view.ImageAnswerPanel', {
 			cls: 'dataview-inline gallery-dataview',
 
 			itemCls: 'thumbnail-image',
-			itemTpl: [
-				'<div class="wrapper">',
-				'<img src="{answerThumbnailImage}"/>',
-				'<span>{answerSubject:htmlEncode}</span>',
-				'</div>'
-			],
+			itemTpl: new Ext.XTemplate(
+				'<tpl if="this.isVertical() === false">',
+					'<div class="wrapper">',
+						'<img src="{answerThumbnailImage}"/>',
+						'<span>{answerSubject:htmlEncode}</span>',
+					'</div>',
+				'</tpl>',
+				'<tpl if="this.isVertical() === true">',
+					'<div class="wrapper">',
+						'<img src="{answerThumbnailImage}"/>',
+						'<span class="answerSubject">{answerSubject:htmlEncode}</span>',
+						'<span class="answerText">{answerText:htmlEncode}</span>',
+					'</div>',
+				'</tpl>',
+				 {
+						isVertical: function() {
+							return me.isVertical;
+						}
+				}
+			),
 			inline:true,
 			scrollable:true,
 			deferEmptyText: false,
 			emptyText: Messages.NO_ANSWERS,
-
 			listeners: {
 				itemtap: function (list, index, element) {
 					var answer = list.getStore().getAt(index).data;
@@ -281,6 +297,9 @@ Ext.define('ARSnova.view.ImageAnswerPanel', {
 		this.imageAnswerList.setCls("dataview-inline gallery-dataview");
 		this.imageAnswerList.setInline({wrap:true});
 		this.imageAnswerList.setScrollable({direction:'vertical'});
+
+		this.isVertical = false;
+		this.imageAnswerList.refresh();
 	},
 	horizontalListClicked: function() {
 		console.log("horizontal clicked");
@@ -288,10 +307,15 @@ Ext.define('ARSnova.view.ImageAnswerPanel', {
 		this.imageAnswerList.setInline({wrap: false});
 		this.imageAnswerList.setScrollable('horizontal');
 
+		this.isVertical = false;
+		this.imageAnswerList.refresh();
 	},
 	verticalListClicked: function() {
 		console.log("vertical clicked");
 		this.imageAnswerList.setCls("dataview-basic gallery-dataview");
 		this.imageAnswerList.setScrollable({direction:'vertical'});
+
+		this.isVertical = true;
+		this.imageAnswerList.refresh();
 	}
 });
