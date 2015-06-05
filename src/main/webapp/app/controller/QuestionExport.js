3a5d2e23035e143fd4a5894433bc8227c6572630
Marco Schaefer <msfr-dev@posteo.de>
2015-06-05 20:45:37 +0200

    Add: Util to parse and unparse json to csv (and backwards)
    Add: Papa-Parse library
    Add: ExportSession controller
    Add: Global var "Papa" to jshint
    
    DG: Amended commit to bump PapaParse dependency.

---
+++ src/main/webapp/app/controller/QuestionExport.js
@@ -0,0 +1,87 @@
+/*
+ * This file is part of ARSnova Mobile.
+ * Copyright (C) 2014 The ARSnova Team
+ *
+ * ARSnova Mobile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ARSnova Mobile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ARSnova Mobile.  If not, see <http://www.gnu.org/licenses/>.
+ */
+Ext.define("ARSnova.controller.QuestionExport", {
+    extend: 'Ext.app.Controller',
+
+	filename: function () {
+		var filename = 'lectureQuestions' + this.getActualDate() + '.csv';
+		return filename;
+	},
+
+    exportCsvTemplate: function() {
+        var csvColumns = ['QuestionType','questionSubject','Question','Answer 1','Answer 2',
+                          'Answer 3','Answer 4','Answer 5','Answer 6','Answer 7','Answer 8',
+                          'Right Answer','\n'].toString();
+
+        var csvMulpipleChoice = ['MC','Subject of a question','The question itself',
+                                 'The 1st answer (mandatory)','The 2nd answer (mandatory)',
+                                 'The 3rd answer (optional)','The 4th answer (optional)',
+                                 'The 5th answer (optional)','The 6th answer (optional)',
+                                 'The 7th answer (optional)','The 8th answer (optional)',
+                                 '"1,2,6"','\n'].toString();
+
+        var csvSingleChoice = ['SC','Subject of a question','The question itself',
+                               'The 1st answer (mandatory)','The 2nd answer (mandatory)',
+                               'The 3rd answer (optional)','The 4th answer (optional)',
+                               'The 5th answer (optional)','The 6th answer (optional)',
+                               'The 7th answer (optional)','The 8th answer (optional)',
+                               '3','\n'].toString();
+
+        var csvFreeText = ['TXT','Subject of a question','The question itself',
+                           '','','','','','','','','','\n'].toString();
+
+        var csvYesNo = ['YN','Subject of a question','The question itself',
+                        '','','','','','','','','y'].toString();
+
+        var csvTemplate = csvColumns + csvMulpipleChoice + csvSingleChoice + csvFreeText + csvYesNo;
+        this.saveFileOnFileSystem(csvTemplate, this.filename());
+    },
+
+    exportCsvFile: function(json) {
+        var csv = ARSnova.utils.CsvUtil.jsonToCsv(json);
+        this.saveFileOnFileSystem(csv, this.filename());
+    },
+    saveFileOnFileSystem: function (csv, filename) {
+        var blob = new Blob([csv], {type: "application/csv;charset=utf-8"});
+        var ua = window.navigator.userAgent;
+        var msie = ua.indexOf("MSIE ");
+
+        if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
+            window.navigator.msSaveBlob(blob, filename);
+        } else {
+            var a = window.document.createElement('a');
+            a.className = "session-export";
+            a.href = window.URL.createObjectURL(blob);
+            a.download = filename;
+
+            // Append anchor to body.
+            document.body.appendChild(a);
+            a.click();
+        }
+        var hTP = ARSnova.app.mainTabPanel.tabPanel.homeTabPanel;
+        hTP.animateActiveItem(hTP.mySessionsPanel, {
+            type: 'slide',
+            direction: 'right',
+            duration: 700
+        });
+        var preparsedQuestion = this.preparseJSONtoCSV(records);
+        var csv = ARSnova.utils.CsvUtil.jsonToCsv(preparsedQuestion);
+        this.saveFileOnFileSystem(csv, this.filename());
+    }
+
+});
---
+++ src/main/webapp/app/utils/CsvUtil.js
@@ -0,0 +1,52 @@
+/*--------------------------------------------------------------------------+
+/*
+ * This file is part of ARSnova Mobile.
+ * Copyright (C) 2014 The ARSnova Team
+ *
+ * ARSnova Mobile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ARSnova Mobile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ARSnova Mobile.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * This utility contains functions to convert json to csv and backwards.
+ * Currently we make use of the Papa-Parse library (see: http://papaparse.com/).
+ * If you want to change the library later on, please do this inside this utility,
+ * so other controllers using csv <-> json coversion won't be affected.
+ */
+Ext.define('ARSnova.utils.CsvUtil', {
+    singleton: true,
+
+    /**
+     * Convert a csv string to json
+     * @param  {String} csv The csv string to parse.
+     * @return {String}     The parsed csv string as json.
+     */
+    csvToJson: function(csv) {
+        if (!csv) { return undefined; }
+        var parsed = Papa.parse(csv);
+        if (parsed.errors.length > 0) { return undefined; }
+        return JSON.stringify(parsed.data);
+    },
+
+    /**
+     * Parse a json object to a csv string.
+     * @param  {Object} json Json object to parse.
+     * @return {String}      Parsed json object as csv string.
+     */
+    jsonToCsv: function(json) {
+        if (!json) { return undefined; }
+        var unparsed = Papa.unparse(json);
+        return unparsed;
+    }
+
+});
