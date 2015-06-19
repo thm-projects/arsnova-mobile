20952301a9473f1cc0b4bdd3d1d15883ee80d277
agrt56 <andreas.gaertner@mni.thm.de>
2015-06-19 02:04:28 +0200

    Fix javascript error in carousel for flashcard questions.

--- src/main/webapp/app/view/Question.js
+++ src/main/webapp/app/view/Question.js
@@ -708,7 +708,9 @@ Ext.define('ARSnova.view.Question', {
 	},
 
 	updateListHeight: function () {
-		if (this.questionObj.questionType !== 'grid' && this.answerList && this.answerList.element) {
+		if (this.questionObj.questionType !== 'grid' &&
+			this.questionObj.questionType !== 'flashcard' &&
+			this.answerList && this.answerList.element) {
 			var listItemsDom = this.answerList.element.select(".x-list .x-inner .x-inner").elements[0];
 			listItemsDom.style.display = 'none';
 			this.answerList.fireEvent('resizeList', this.answerList.element);
