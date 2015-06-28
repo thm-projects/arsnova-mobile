thm-projects/arsnova-mobile

commit 8f5845fb9167810d798061a0e11420c3e7e06e7f

Fix some XSS vulnerabilities concerning titlebar title.

src/main/webapp/app/view/feedback/StatisticPanel.js
@@ -310,6 +310,6 @@ Ext.define('ARSnova.view.feedback.StatisticPanel', {
 
 	checkTitle: function () {
 		var title = Ext.util.Format.htmlEncode(localStorage.getItem('shortName'));
-		this.toolbar.setTitle(title);
+		this.toolbar.setTitle(Ext.util.Format.htmlEncode(title));
 	}
 });

src/main/webapp/app/view/FreetextAnswerPanel.js
@@ -72,6 +72,7 @@ Ext.define('ARSnova.view.FreetextAnswerPanel', {
 			text: Messages.BACK,
 			ui: 'back',
 			scope: this,
+			align: 'left',
 			handler: function () {
 				var object, me = this;
 				var tabPanel = ARSnova.app.mainTabPanel.tabPanel;
@@ -110,19 +111,11 @@ Ext.define('ARSnova.view.FreetextAnswerPanel', {
 			}
 		});
 
-		this.toolbar = Ext.create('Ext.Toolbar', {
-			cls: 'answerStatisticToolbar',
+		this.toolbar = Ext.create('Ext.TitleBar', {
 			docked: 'top',
 			ui: 'light',
-			items: [this.backButton, {
-				xtype: 'spacer'
-			}, {
-				flex: 99,
-				xtype: 'title',
-				title: this.questionObj.subject
-			}, {
-				xtype: 'spacer'
-			}]
+			title: Ext.util.Format.htmlEncode(this.questionObj.subject),
+			items: [this.backButton]
 		});
 
 		// Create standard panel with framework support

src/main/webapp/app/view/speaker/QuestionStatisticChart.js
@@ -97,6 +97,7 @@ Ext.define('ARSnova.view.speaker.QuestionStatisticChart', {
 			text: Messages.BACK,
 			ui: 'back',
 			scope: this,
+			align: 'left',
 			style: 'min-width: 60px;',
 			handler: function () {
 				var object, me = this;
@@ -137,73 +138,25 @@ Ext.define('ARSnova.view.speaker.QuestionStatisticChart', {
 		});
 
 		this.answerCounter = Ext.create('Ext.Component', {
-			cls: "x-toolbar-title alignRight counterText"
+			cls: "x-toolbar-title alignRight counterText",
+			align: 'right'
 		});
 
-		this.toolbar = Ext.create('Ext.Toolbar', {
+		this.toolbar = Ext.create('Ext.TitleBar', {
 			docked: 'top',
 			ui: 'light',
-			cls: 'answerStatisticToolbar',
-			items: [this.backButton, {
-				xtype: 'spacer'
-			}, {
-				flex: 99,
-				xtype: 'title',
-				title: this.questionObj.subject
-			}, {
-				xtype: 'spacer'
-			}, this.answerCounter, {
+			title: Ext.util.Format.htmlEncode(this.questionObj.subject),
+			items: [
+				this.backButton,
+				this.answerCounter, {
 				xtype: 'button',
+				align: 'right',
 				iconCls: 'icon-check',
 				cls: 'toggleCorrectButton',
-				handler: function (button) {
-					var me = this,
-					data = [];
-
-					button.disable();
-
-					if (this.toggleCorrect) {
-						button.removeCls('x-button-pressed');
-					} else {
-						button.addCls('x-button-pressed');
-					}
-					this.toggleCorrect = !this.toggleCorrect;
-
-					// remove all data for a smooth "redraw"
-					this.questionStore.each(function (record) {
-						data.push({
-							text: record.get('text'),
-							"value-round1": record.get('value-round1'),
-							"value-round2": record.get('value-round2'),
-							"percent-round1": record.get('percent-round1'),
-							"percent-round2": record.get('percent-round2')
-						});
-
-						record.set('value-round1', 0);
-						record.set('value-round2', 0);
-						record.set('percent-round1', 0);
-						record.set('percent-round2', 0);
-					});
-
-					var updateDataTask = Ext.create('Ext.util.DelayedTask', function () {
-						me.questionStore.setData(data);
-						button.enable();
-					});
-
-					var setGradientTask = Ext.create('Ext.util.DelayedTask', function () {
-						// updates the chart's colors
-						me.setGradients();
-
-						// delay till chart is redrawn
-						updateDataTask.delay(me.chartRefreshDuration - 200);
-					});
-
-					// delay till chart is empty
-					setGradientTask.delay(me.chartRefreshDuration - 200);
-				},
 				scope: this,
+				handler: this.toggleCorrectHandler,
 				hidden: !hasCorrectAnswers() || this.questionObj.questionType === 'grid' ||
-						(ARSnova.app.userRole === ARSnova.app.USER_ROLE_STUDENT && !this.questionObj.showAnswer)
+					(ARSnova.app.userRole === ARSnova.app.USER_ROLE_STUDENT && !this.questionObj.showAnswer)
 			}]
 		});
 
@@ -244,17 +197,17 @@ Ext.define('ARSnova.view.speaker.QuestionStatisticChart', {
 			]
 		});
 
-		this.titlebar = Ext.create('ARSnova.view.MathJaxMarkDownPanel', {
+		this.titlePanel = Ext.create('ARSnova.view.MathJaxMarkDownPanel', {
 			cls: 'questionStatisticTitle',
 			hideMediaElements: true,
 			baseCls: Ext.baseCSSPrefix + 'title',
 			docked: 'top',
 			style: ''
 		});
-		this.titlebar.setContent(this.questionObj.text, true, true);
+		this.titlePanel.setContent(this.questionObj.text, true, true);
 
 		if (this.questionObj.questionType === "grid") {
-			this.titlebar = Ext.create('Ext.Toolbar', {
+			this.titlePanel = Ext.create('Ext.Toolbar', {
 				cls: 'questionStatisticTitle',
 				docked: 'top',
 				title: '',
@@ -427,7 +380,7 @@ Ext.define('ARSnova.view.speaker.QuestionStatisticChart', {
 				this.toolbar,
 				this.countdownTimer,
 				this.piToolbar,
-				this.titlebar,
+				this.titlePanel,
 				this.questionChart
 			]);
 		} else {
@@ -441,7 +394,7 @@ Ext.define('ARSnova.view.speaker.QuestionStatisticChart', {
 				xtype: 'formpanel',
 				style: 'margin-top: 10px',
 				scrollable: null,
-				items: [this.titlebar,
+				items: [this.titlePanel,
 						this.countdownTimer,
 						this.contentField,
 						this.questionChart,
@@ -781,6 +734,52 @@ Ext.define('ARSnova.view.speaker.QuestionStatisticChart', {
 		} else {
 			this.hasAnswers = this.answerCountSecondRound > 0;
 		}
+	},
+
+	toggleCorrectHandler: function (button) {
+		var me = this,
+		data = [];
+
+		button.disable();
+
+		if (this.toggleCorrect) {
+			button.removeCls('x-button-pressed');
+		} else {
+			button.addCls('x-button-pressed');
+		}
+		this.toggleCorrect = !this.toggleCorrect;
+
+		// remove all data for a smooth "redraw"
+		this.questionStore.each(function (record) {
+			data.push({
+				text: record.get('text'),
+				"value-round1": record.get('value-round1'),
+				"value-round2": record.get('value-round2'),
+				"percent-round1": record.get('percent-round1'),
+				"percent-round2": record.get('percent-round2')
+			});
+
+			record.set('value-round1', 0);
+			record.set('value-round2', 0);
+			record.set('percent-round1', 0);
+			record.set('percent-round2', 0);
+		});
+
+		var updateDataTask = Ext.create('Ext.util.DelayedTask', function () {
+			me.questionStore.setData(data);
+			button.enable();
+		});
+
+		var setGradientTask = Ext.create('Ext.util.DelayedTask', function () {
+			// updates the chart's colors
+			me.setGradients();
+
+			// delay till chart is redrawn
+			updateDataTask.delay(me.chartRefreshDuration - 200);
+		});
+
+		// delay till chart is empty
+		setGradientTask.delay(me.chartRefreshDuration - 200);
 	},
 
 	showEmbeddedPagePreview: function (embeddedPage) {

src/main/webapp/app/view/speaker/ShowcaseQuestionPanel.js
@@ -179,7 +179,7 @@ Ext.define('ARSnova.view.speaker.ShowcaseQuestionPanel', {
 			panel.zoomButton.setHidden(screenWidth < 700);
 			newQuestion.setZoomLevel(ARSnova.app.globalZoomLevel);
 			newQuestion.updateQuestionText();
-			this.toolbar.setTitle(title);
+			this.toolbar.setTitle(Ext.util.Format.htmlEncode(title));
 		}
 	},
 

src/main/webapp/app/view/user/QuestionPanel.js
@@ -85,8 +85,8 @@ Ext.define('ARSnova.view.user.QuestionPanel', {
 
 	onItemChange: function (panel, newQuestion, oldQuestion) {
 		if (newQuestion.questionObj) {
-			this.toolbar.setTitle(newQuestion.getQuestionTypeMessage());
 			newQuestion.updateQuestionText();
+			this.toolbar.setTitle(Ext.util.Format.htmlEncode(newQuestion.getQuestionTypeMessage()));
 		}
 	},
 

src/main/webapp/resources/sass/app/_general.scss
@@ -192,16 +192,6 @@
 	border: none !important;
 }
 
-.answerStatisticToolbar {
-	.x-toolbar-inner {
-		display: flex !important;
-
-		.x-title {
-			flex: 1;
-		}
-	}
-}
-
 .carouselNavigationElement {
 	top: 0;
 	bottom: 0;

commit 1ca094bb1065c09cef3ba65dae27b68640faa860

Do not encode feedback panel's title twice
Fixes regression introduced in 8f5845fb9167810d798061a0e11420c3e7e06e7f.

src/main/webapp/app/view/feedback/StatisticPanel.js
@@ -309,7 +309,7 @@ Ext.define('ARSnova.view.feedback.StatisticPanel', {
 	},
 
 	checkTitle: function () {
-		var title = Ext.util.Format.htmlEncode(localStorage.getItem('shortName'));
+		var title = localStorage.getItem('shortName');
 		this.toolbar.setTitle(Ext.util.Format.htmlEncode(title));
 	}
 });