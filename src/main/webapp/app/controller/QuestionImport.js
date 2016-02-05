14bb496bbe5484a83795470eff05d963b4678879
Daniel Gerhardt <code@dgerhardt.net>
2016-02-05 21:55:46 +0100

    Update copyright year

--- src/main/webapp/app/controller/QuestionImport.js
+++ src/main/webapp/app/controller/QuestionImport.js
@@ -1,7 +1,7 @@
 /*
  * This file is part of ARSnova Mobile.
  * Copyright (C) 2011-2012 Christian Thomas Weber
- * Copyright (C) 2012-2015 The ARSnova Team
+ * Copyright (C) 2012-2016 The ARSnova Team
  *
  * ARSnova Mobile is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
