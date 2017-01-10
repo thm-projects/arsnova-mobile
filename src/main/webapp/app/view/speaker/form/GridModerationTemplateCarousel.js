0f299d15f230eaa0405bfddcb805acbea6c5c49b
Daniel Gerhardt <code@dgerhardt.net>
2017-01-10 14:21:34 +0100

    Update copyright year and add missing file headers

--- src/main/webapp/app/view/speaker/form/GridModerationTemplateCarousel.js
+++ src/main/webapp/app/view/speaker/form/GridModerationTemplateCarousel.js
@@ -1,4 +1,7 @@
 /*
+ * This file is part of ARSnova Mobile.
+ * Copyright (C) 2011-2012 Christian Thomas Weber
+ * Copyright (C) 2012-2017 The ARSnova Team
  *
  * ARSnova Mobile is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/main/webapp/app/view/speaker/form/ImportQuestion.js
+++ src/main/webapp/app/view/speaker/form/ImportQuestion.js
@@ -1,3 +1,21 @@
+/*
+ * This file is part of ARSnova Mobile.
+ * Copyright (C) 2011-2012 Christian Thomas Weber
+ * Copyright (C) 2012-2017 The ARSnova Team
+ *
+ * ARSnova Mobile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ARSnova Mobile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ARSnova Mobile.  If not, see <http://www.gnu.org/licenses/>.
+ */
 Ext.define('ARSnova.view.speaker.form.ImportQuestion', {
 	extend: 'Ext.Container',
 
